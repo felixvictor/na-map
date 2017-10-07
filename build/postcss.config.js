298abb67dac43cca7a931fdbf494020c2db27bce
Felix Victor <felix.victor.na@gmail.com>
2017-10-07 18:10:32 +0200

    stylelint

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
 if ("prod" === process.env.NODE_ENV) {
     module.exports.plugins.push(
         require("postcss-clean")({
-            level: { 1: { specialComments: 0 } },
+            level: { 1: { specialComments: 0 } }
         })
     );
 }
