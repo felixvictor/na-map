f261ed52be22a86c180448bbbb5fbf05623443a1
Felix Victor <felix.victor.na@gmail.com>
2017-10-31 10:52:27 +0100

    google analytics

---
+++ src/js/analytics.js
@@ -0,0 +1,12 @@
+// analytics.js
+
+export default function naAnalytics() {
+    /*
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "UA-108632741-1");
+    */
+}
