113b2f65bb1331de6475578495ca0b5771f3d878
Felix Victor <felix.victor.na@gmail.com>
2017-11-10 17:23:28 +0100

    push

--- build/common.sh
+++ build/common.sh
@@ -13,7 +13,7 @@ function change-port-data () {
 
     # http://api.shipsofwar.net/servers?apikey=1ZptRtpXAyEaBe2SEp63To1aLmISuJj3Gxcl5ivl&callback=setActiveRealms
     for SERVER_NAME in "eu1" "eu2" "us2"; do
-        GIT_FILE="public/${SERVER_NAME}.json"
+        GIT_FILE="$(pwd)/public/${SERVER_NAME}.json"
         API_FILE="$(pwd)/API-${SERVER_NAME}-${DATE}.json"
         get-file "${SERVER_NAME}" "${API_FILE}"
         nodejs build/change-port-data.js "${GIT_FILE}" "${API_FILE}"
