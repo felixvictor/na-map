7ab30e6bc12ebeb37f2aad8804a16e3c23dc45e8
Felix Victor <felix.victor.na@gmail.com>
2017-11-14 20:20:09 +0100

    keine source-map

--- webpack.config.js
+++ webpack.config.js
@@ -254,7 +254,7 @@ let config = {
 };
 
 if ("prod" === process.env.NODE_ENV) {
-    config.devtool = "source-map";
+    config.devtool = "";
     config.plugins.push(
         new MinifyPlugin({
             mangle: { topLevel: true },
