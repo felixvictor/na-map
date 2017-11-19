d630896295266e04cab9dbd05b68d5cfea35f357
Felix Victor <felix.victor.na@gmail.com>
2017-11-19 11:17:12 +0100

    bootstrap import

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,6 @@
 import naDisplay from "./na-display";
 import naAnalytics from "./analytics.js";
 import "bootstrap/js/dist/modal";
-import "bootstrap/js/dist/tooltip";
 import "bootstrap/js/dist/util";
 
 function main() {
