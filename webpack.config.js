0c84445cedb9c93372bf1304557b9a106f8bafac
Felix Victor <felix.victor.na@gmail.com>
2018-01-17 17:19:04 +0100

    yarn

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ const babelOpt = {
         [
             "@babel/env",
             {
+                //debug: true,
                 exclude: ["transform-typeof-symbol"],
                 loose: true,
                 modules: false,
