7bf3b898dbb093a844bc75472f013a8cc7c5972e
Felix Victor <felix.victor.na@gmail.com>
2018-02-17 10:06:21 +0100

    tiles: make-tiles with sharp.js
    Former-commit-id: 8644bf3fec39a300f91a0f8d76801db6895b5a4a [formerly 4dd57b859bf846657ac12ee43a13f367f9a934e8]
    Former-commit-id: 44973200301029897abec7d8b5e6a9bfc42a0262

---
+++ build/make-tiles.mjs
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import sharp from "sharp";
+
+const inFilename = process.argv[2],
+    tileSize = 256,
+    outPath = "src/images/map";
+
+sharp(inFilename)
+    .jpeg({
+        quality: 95,
+        trellisQuantisation: true,
+        overshootDeringing: true,
+        optimiseScans: true
+    })
+    .tile({ size: tileSize, layout: "google" })
+    .toFile(outPath);
