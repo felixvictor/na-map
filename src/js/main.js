472b9958368a2e9d66177aded447236777c40b70
Felix Victor <felix.victor.na@gmail.com>
2018-04-01 15:17:44 +0200

    Select menu re-arranged wip

--- src/js/main.js
+++ src/js/main.js
@@ -32,6 +32,7 @@ function main() {
     function setupListener() {
         // Adapted https://github.com/bootstrapthemesco/bootstrap-4-multi-dropdown-navbar
         $(".dropdown-menu a.dropdown-toggle").on("click", event => {
+            console.log("old event");
             const $el = $(event.currentTarget);
             const $parent = $el.offsetParent(".dropdown-menu");
 
@@ -40,14 +41,17 @@ function main() {
                     .parents(".dropdown-menu")
                     .first()
                     .find(".show")
+                  .not(".inner")
                     .removeClass("show");
             }
             $el.next(".dropdown-menu").toggleClass("show");
             $el.parent("li").toggleClass("show");
             $el.parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", event2 => {
-                $(event2.currentTarget)
+                const el2=$(event2.currentTarget)
                     .find(".dropdown-menu .show")
-                    .removeClass("show");
+                  .not(".inner");
+                console.log(el2);
+                    el2.removeClass("show");
             });
             if (!$parent.parent().hasClass("navbar-nav")) {
                 $el.next().css({ top: $el[0].offsetTop, left: $parent.outerWidth() - 4 });
