d30b68de2f9ef8ed453dc70835ceee419fb2c0cc
Felix Victor <felix.victor.na@gmail.com>
2018-04-09 15:25:31 +0200

    grid wip
    Former-commit-id: e3c118b8144bcb280d4b7d3a229b99c13bf50b7e [formerly cdbad236639a2c48273d8dc8f0ca62ab4b7dbfa6]
    Former-commit-id: 6e916d7ad7cf402c7b946291924fb6a2a962a74b

--- src/js/na-display.js
+++ src/js/na-display.js
@@ -15,6 +15,7 @@ import "bootstrap/js/dist/tooltip";
 import "bootstrap/js/dist/util";
 
 import { nearestPow2, checkFetchStatus, getJsonFromFetch, putFetchError } from "./util";
+import { convertCoordX, convertCoordY, convertInvCoordX, convertInvCoordY } from "./common";
 
 import Course from "./course";
 import F11 from "./f11";
@@ -58,6 +59,7 @@ export default function naDisplay(serverName) {
             this._radioButton = "compass";
 
             this._setupSvg();
+            this._setupGrid();
             this._setupListener();
         }
 
@@ -109,36 +111,39 @@ export default function naDisplay(serverName) {
         }
 
         _setupGrid() {
-            const xScale = d3.scale
-                .linear()
-                .domain([-width / 2, width / 2])
-                .range([0, width]);
+            this._xScale = d3
+                .scaleLinear()
+                .domain([
+                    convertInvCoordX(this.coord.max, this.coord.max),
+                    convertInvCoordX(this.coord.min, this.coord.min)
+                ])
+                .range([this.coord.min, this.coord.max]);
+            this._yScale = d3
+                .scaleLinear()
+                .domain([
+                    convertInvCoordY(this.coord.max, this.coord.max),
+                    convertInvCoordY(this.coord.min, this.coord.min)
+                ])
+                .range([this.coord.min, this.coord.max]);
 
-            const yScale = d3.scale
-                .linear()
-                .domain([-height / 2, height / 2])
-                .range([height, 0]);
+            this._xAxis = d3
+                .axisBottom(this._xScale)
+                .ticks(this.coord.max / 128)
+                .tickSize(this._height / 2)
+                .tickPadding(this._height);
+            this._yAxis = d3
+                .axisRight(this._yScale)
+                .ticks(this.coord.max / 128)
+                .tickSize(this._width);
 
-            const xAxis = d3.svg
-                .axis()
-                .scale(xScale)
-                .orient("bottom")
-                .tickSize(-height);
-
-            const yAxis = d3.svg
-                .axis()
-                .scale(yScale)
-                .orient("left")
-                .ticks(5)
-                .tickSize(-width);
             this._gXAxis = this._svg
                 .append("g")
-                .classed("grid", true)
-                .call(xAxis);
+                .classed("axis axis-x", true)
+                .call(this._xAxis);
             this._gYAxis = this._svg
                 .append("g")
-                .classed("grid", true)
-                .call(yAxis);
+                .classed("axis axis-y", true)
+                .call(this._yAxis);
         }
 
         _displayMap(transform) {
@@ -255,7 +260,7 @@ export default function naDisplay(serverName) {
             teleport.setZoomLevel(zoomLevel);
         }
 
-        static _updateCurrent() {
+        _updateCurrent() {
             pbZone.refresh();
             teleport.setTeleportData();
             teleport.updateTeleportAreas();
@@ -266,16 +271,16 @@ export default function naDisplay(serverName) {
             if (d3.event.transform.k > this._PBZoneZoomThreshold) {
                 if (this._zoomLevel !== "pbZone") {
                     this._setZoomLevel("pbZone");
-                    Map._updateCurrent();
+                    this._updateCurrent();
                 }
             } else if (d3.event.transform.k > this._labelZoomThreshold) {
                 if (this._zoomLevel !== "portLabel") {
                     this._setZoomLevel("portLabel");
-                    Map._updateCurrent();
+                    this._updateCurrent();
                 }
             } else if (this._zoomLevel !== "initial") {
                 this._setZoomLevel("initial");
-                Map._updateCurrent();
+                this._updateCurrent();
             }
         }
 
@@ -288,7 +293,8 @@ export default function naDisplay(serverName) {
                 .scale(Math.round(d3.event.transform.k * 1000) / 1000);
 
             this._displayMap(zoomTransform);
-            this._gXAxis.attr("transform", zoomTransform);
+            this._gXAxis.call(this._xAxis.scale(d3.event.transform.rescaleX(this._xScale)));
+            this._gYAxis.call(this._yAxis.scale(d3.event.transform.rescaleY(this._yScale)));
             ports.transform(zoomTransform);
             teleport.transform(zoomTransform);
             course.transform(zoomTransform);
