e62f3db9d8f74fa08f9d1524f9361ab12e53eb3b
Felix Victor <felix.victor.na@gmail.com>
2018-04-11 11:33:23 +0200

    documentation.js, jsdoc added

--- src/js/grid.js
+++ src/js/grid.js
@@ -1,4 +1,9 @@
-/*
-    grid.js
+/**
+ * This file is part of na-map.
+ *
+ * @file Display grid.
+ * @module grid
+ * @author iB aka Felix Victor
+ * @copyright 2017, 2018
+ * @license   http://www.gnu.org/licenses/gpl.html
  */
-
