890c437e2ab21e7a9594ffe0ce015fe4a2a85a45
Felix Victor <felix.victor.na@gmail.com>
2018-05-10 18:54:21 +0200

    port text dx/dy finalised
    Former-commit-id: 52a1d75cbfb25759ca56f76ef38013078e089207 [formerly afd1388bdfb6e94456a731744dda007792fef3b7]
    Former-commit-id: 439b7a869a51b738738f233ea3ade129790e485c

--- build/common.mjs
+++ build/common.mjs
@@ -56,3 +56,66 @@ export function saveJson(fileName, data) {
 }
 
 export const readJson = fileName => JSON.parse(fs.readFileSync(fileName, "utf8"));
+
+/**
+ * Convert radians to degrees (see {@link http://cwestblog.com/2012/11/12/javascript-degree-and-radian-conversion/})
+ * @function
+ * @param {Number} radians - Radians
+ * @return {Number} Degrees
+ */
+Math.radiansToDegrees = radians => radians * 180 / Math.PI;
+
+/**
+ * @typedef {Array} Point
+ * @property {number} 0 - X Coordinate
+ * @property {number} 1 - Y Coordinate
+ */
+
+/**
+ * Calculate the angle in degrees between two points
+ * @see https://stackoverflow.com/questions/9970281/java-calculating-the-angle-between-two-points-in-degrees
+ * @function
+ * @param {Point} centerPt - Center point
+ * @param {Point} targetPt - Target point
+ * @return {Number} Degrees between centerPt and targetPt
+ */
+export const rotationAngleInDegrees = (centerPt, targetPt) => {
+    let theta = Math.atan2(targetPt[1] - centerPt[1], targetPt[0] - centerPt[0]);
+    theta -= Math.PI / 2.0;
+    const degrees = Math.radiansToDegrees(theta);
+    return (degrees + 360) % 360;
+};
+
+/**
+ * Calculate the angle in degrees between two points
+ * @see https://stackoverflow.com/questions/9970281/java-calculating-the-angle-between-two-points-in-degrees
+ * @function
+ * @param {Point} centerPt - Center point
+ * @param {Point} targetPt - Target point
+ * @return {Number} Degrees between centerPt and targetPt
+ */
+export const rotationAngleInRadians = (centerPt, targetPt) =>
+    Math.atan2(centerPt[1], centerPt[0]) - Math.atan2(targetPt[1], targetPt[0]);
+
+/**
+ * Calculate the distance between two points
+ * @see https://www.mathsisfun.com/algebra/distance-2-points.html
+ * @function
+ * @param {Point} centerPt - Center point
+ * @param {Point} targetPt - Target point
+ * @return {Number} Distance between centerPt and targetPt
+ */
+export const distancePoints = (centerPt, targetPt) =>
+    Math.sqrt((centerPt[0] - targetPt[0]) ** 2 + (centerPt[1] - targetPt[1]) ** 2);
+
+
+/**
+ * Convert degrees to radians
+ * @function
+ * @param {Number} degrees - Degrees
+ * @return {Number} Radians
+ */
+export const degreesToRadians = degrees => Math.PI / 180 * (degrees - 90);
+
+export const defaultFontSize = 16;
+export const defaultCircleSize = 16;
