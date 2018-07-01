d4dea9d43edeca973e38370056fa113395d89bac
Felix Victor <felix.victor.na@gmail.com>
2018-07-01 10:53:43 +0200

    modules list: join ship rates when properties are the same
    Former-commit-id: 5c1357640af290b68394925274da0ec8e35b29f4 [formerly bc744419e55f25ccf6fe38976d7b516e911703aa]
    Former-commit-id: 6fe1956da0af332179066676655ce96a67d3270f

--- src/js/module-list.js
+++ src/js/module-list.js
@@ -42,40 +42,57 @@ export default class Module {
             .selectpicker({ noneSelectedText: "Select module type" });
     }
 
-    _getText(moduleType) {
+    /**
+     * Get rows with modules for module moduleType
+     * @param {String} moduleType Module Type
+     * @return {Array} html table rows
+     * @private
+     */
+    _getRows(moduleType) {
+        /**
+         * Rate code mapped into human readable string
+         * @type {Map<String, String>}
+         */
         const rates = new Map([
             ["L", `${getOrdinal(1)}\u202f\u2013\u202f${getOrdinal(3)}`],
             ["M", `${getOrdinal(4)}\u202f\u2013\u202f${getOrdinal(5)}`],
             ["S", `${getOrdinal(6)}\u202f\u2013\u202f${getOrdinal(7)}`]
         ]);
 
-        function getRate(module) {
-            return module.moduleLevel !== "U" ? ` ${rates.get(module.moduleLevel)}` : "";
+        /**
+         * Get ship rate from module level
+         * @param {Object} moduleLevel Module level
+         * @return {string} Ship rate
+         */
+        function getRate(moduleLevel) {
+            return moduleLevel !== "U" ? ` ${rates.get(moduleLevel)}` : "";
         }
-        let text = '<table class="table table-sm table-striped modules small mt-4"><thead>';
-        text += "<tr>";
-        text += "<tr><th><em>Module</em></th><th><em>Ship rate</em></th><th><em>Modifier</em></th></tr></thead><tbody>";
 
         let rate = "";
+        const rows = [];
         this._moduleData.forEach(type => {
             if (type[0] === moduleType) {
                 type[1].forEach((module, i) => {
-                    rate = getRate(module);
-                    if (
-                        i + 1 < type[1].length &&
-                        module.name === type[1][i + 1].name &&
-                        JSON.stringify(module.properties) === JSON.stringify(type[1][i + 1].properties)
-                    ) {
+                    /**
+                     * Test if current module and module at index position has same properties
+                     * @param {integer} index Position
+                     * @return {boolean} True if same
+                     */
+                    function hasSameProperties(index) {
+                        return (
+                            index < type[1].length &&
+                            module.name === type[1][index].name &&
+                            JSON.stringify(module.properties) === JSON.stringify(type[1][index].properties)
+                        );
+                    }
+
+                    rate = getRate(module.moduleLevel);
+                    if (hasSameProperties(i + 1)) {
                         // eslint-disable-next-line no-param-reassign
                         type[1][i + 1].hasSamePropertiesAsPrevious = true;
                         rate += `<br>${getRate(type[1][i + 1])}`;
                     }
-                    if (
-                        i + 2 < type[1].length &&
-                        module.name === type[1][i + 2].name &&
-                        JSON.stringify(module.properties) === JSON.stringify(type[1][i + 2].properties)
-                        // eslint-disable-next-line no-param-reassign
-                    ) {
+                    if (hasSameProperties(i + 2)) {
                         // eslint-disable-next-line no-param-reassign
                         type[1][i + 2].hasSamePropertiesAsPrevious = true;
                         rate = "";
@@ -84,27 +101,55 @@ export default class Module {
                         typeof module.hasSamePropertiesAsPrevious === "undefined" ||
                         !module.hasSamePropertiesAsPrevious
                     ) {
-                        text += `<tr><td>${module.name}</td><td>${rate}</td><td>${module.properties
-                            .map(property => {
-                                const amount = property.absolute
-                                    ? property.amount
-                                    : formatSignPercent(property.amount / 100);
-                                return `${property.modifier} ${amount}`;
-                            })
-                            .join("<br>")}</td></tr>`;
+                        rows.push(
+                            `<tr><td>${module.name}</td><td>${rate}</td><td>${module.properties
+                                .map(property => {
+                                    const amount = property.absolute
+                                        ? property.amount
+                                        : formatSignPercent(property.amount / 100);
+                                    return `${property.modifier} ${amount}`;
+                                })
+                                .join("<br>")}</td></tr>`
+                        );
                     }
                 });
             }
         });
+        return rows;
+    }
+
+    /**
+     * Construct module list tables
+     * @param {string} moduleType Module type.
+     * @return {string} html string
+     * @private
+     */
+    _getText(moduleType) {
+        let text = '<table class="table table-sm table-striped modules small mt-4"><thead>';
+        text += "<tr>";
+        text += "<tr><th><em>Module</em></th><th><em>Ship rate</em></th><th><em>Modifier</em></th></tr></thead><tbody>";
+
+        const rows = this._getRows(moduleType);
+        text += rows.join("");
         text += "</tbody></table>";
         return text;
     }
 
+    /**
+     * Show modules for selected module type
+     * @param {Object} event Event
+     * @return {void}
+     * @private
+     */
     _moduleSelected(event) {
         const moduleType = $(event.currentTarget)
             .find(":selected")
             .val();
+
+        // Remove old module list
         d3.select(`${this._div} div`).remove();
+
+        // Add new module list
         d3.select(this._div).append("div");
         $(this._div)
             .find("div")
