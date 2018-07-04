5d900bc6d907a4fc4f0ec4c03f19d74d3d840fe8
natopo <felix.victor.na@gmail.com>
2018-07-04 07:11:21 +0200

    squash! push
    Former-commit-id: 24180f10fe5421f796fce212d6694ebfd9213e1a

--- build/port-data.sh
+++ build/port-data.sh
@@ -57,7 +57,7 @@ function get_git_update () {
     BASE=$(git merge-base @ "@{u}")
 
     if [ "${LOCAL}" == "${BASE}" ]; then
-        git pull --quiet
+        git pull
     fi
 }
 
@@ -165,8 +165,8 @@ function log_date () {
 }
 
 function update_tweets () {
-    get_git_update
     cd ${BASE_DIR}
+    get_git_update
     get_tweets
     if update_ports; then
         copy_data
