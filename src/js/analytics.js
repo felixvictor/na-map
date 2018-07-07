43a8b2a7a2541dd29862d4caa75e626da04a1eca
Felix Victor <felix.victor.na@gmail.com>
2018-07-07 13:30:09 +0200

    ga click
    Former-commit-id: 363eb3ef0b3bdc873c75ed6dddfc728303a0c5e4

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,11 +1,12 @@
 // analytics.js
 
-export default function naAnalytics() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        dataLayer.push(arguments);
-    }
+window.dataLayer = window.dataLayer || [];
 
+export function gtag() {
+    window.dataLayer.push(arguments);
+}
+
+export default function naAnalytics() {
     gtag("js", new Date());
     gtag("config", "UA-109520372-1");
 }
