11a3cc2abdf48a1537243c66466d1f997c92c172
Felix Victor <felix.victor.na@gmail.com>
2018-07-07 13:47:05 +0200

    squash! push
    Former-commit-id: d693cec0c9e9e05e4bde938dbcc922723b91c353

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -2,11 +2,34 @@
 
 window.dataLayer = window.dataLayer || [];
 
-export function gtag() {
+/**
+ * Google tag manager
+ * @return {void}
+ */
+function gtag() {
     window.dataLayer.push(arguments);
 }
 
-export default function naAnalytics() {
+/**
+ * Register event
+ * @param {string} category Event category
+ * @param {string} label Event label
+ * @return {void}
+ */
+export function registerEvent(category, label) {
+    if (typeof window.google_tag_manager !== "undefined" && window.google_tag_manager.dataLayer.gtmLoad) {
+        gtag("event", "click", {
+            event_category: category,
+            event_label: label
+        });
+    }
+}
+
+/**
+ * Init google tag manager
+ * @return {void}
+ */
+export default function initAnalytics() {
     gtag("js", new Date());
     gtag("config", "UA-109520372-1");
 }
