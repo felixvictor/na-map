9cd7c249ef4934c0831c61a2d2eb6300509cb6b9
Felix Victor <felix.victor.na@gmail.com>
2018-07-26 19:02:58 +0200

    join circles wip

--- src/js/pbzone.js
+++ src/js/pbzone.js
@@ -8,37 +8,21 @@
 import Cookies from "js-cookie";
 
 export default class PBZone {
-    constructor(pbData, ports) {
-        function getFeature(d) {
-            return {
-                type: "Feature",
-                id: d.id,
-                geometry: d.geometry
-            };
-        }
-
+    constructor(pbCircles, forts, towers, joinCircles, ports) {
         this._ports = ports;
 
-        // Port ids of capturable ports
-        const portIds = this._ports.portData.filter(port => !port.properties.nonCapturable).map(port => port.id);
-console.log(pbData);
-        this._pbCircleDataDefault = pbData.pbCircles.features
-            .filter(port => portIds.includes(port.id))
-            .map(d => getFeature(d));
-        this._pbCircleData = this._pbCircleDataDefault;
+        console.log("constructor pbCircles", pbCircles);
+        this._pbCircleDataDefault = pbCircles;
+        this._pbCircleData = pbCircles;
 
-        this._fortDataDefault = pbData.forts.features.filter(port => portIds.includes(port.id)).map(d => getFeature(d));
-        this._fortData = this._fortDataDefault;
+        this._fortDataDefault = forts;
+        this._fortData = forts;
 
-        this._towerDataDefault = pbData.towers.features
-            .filter(port => portIds.includes(port.id))
-            .map(d => getFeature(d));
-        this._towerData = this._towerDataDefault;
+        this._towerDataDefault = towers;
+        this._towerData = towers;
 
-        this._joinCircleDataDefault = pbData.joinCircles.features
-            .filter(port => portIds.includes(port.id))
-            .map(d => getFeature(d));
-        this._joinCircleData = this._joinCircleDataDefault;
+        this._joinCircleDataDefault = joinCircles;
+        this._joinCircleData = joinCircles;
 
         /**
          * showLayer cookie name
@@ -68,13 +52,13 @@ console.log(pbData);
     _setupSvg() {
         this._g = d3
             .select("#na-svg")
-            .append("g")
+            .insert("g", "g.ports")
             .classed("pb", true);
+        this._gJoinCirclesInner = this._g.append("path").classed("join-circle", true);
+        this._gJoinCirclesOuter = this._g.append("path").classed("join-circle", true);
         this._gPBCircles = this._g.append("path").classed("pb-circle", true);
         this._gTowers = this._g.append("path").classed("tower", true);
         this._gForts = this._g.append("path").classed("fort", true);
-        this._gJoinCirclesInner = this._g.append("path").classed("join-circle", true);
-        this._gJoinCirclesOuter = this._g.append("path").classed("join-circle", true);
     }
 
     _setupListener() {
@@ -119,11 +103,11 @@ console.log(pbData);
     }
 
     _update() {
-        this._gPBCircles.datum(this._pbData).attr("d", d3.geoPath().pointRadius(4));
+        this._gPBCircles.datum(this._pbCircleData).attr("d", d3.geoPath().pointRadius(4));
         this._gTowers.datum(this._towerData).attr("d", d3.geoPath().pointRadius(1.5));
         this._gForts.datum(this._fortData).attr("d", d3.geoPath().pointRadius(2));
-        this._gJoinCirclesInner.datum(this._joinCircleData).attr("d", d3.geoPath().pointRadius(8));
-        this._gJoinCirclesOuter.datum(this._joinCircleData).attr("d", d3.geoPath().pointRadius(16));
+        this._gJoinCirclesInner.datum(this._joinCircleData).attr("d", d3.geoPath().pointRadius(16));
+        this._gJoinCirclesOuter.datum(this._joinCircleData).attr("d", d3.geoPath().pointRadius(32));
     }
 
     _isPortIn(d) {
@@ -157,7 +141,6 @@ console.log(pbData);
     }
 
     refresh() {
-        console.log("pbData refresh (sollte nicht zu häufig passieren");
         this._setData();
         this._update();
     }
