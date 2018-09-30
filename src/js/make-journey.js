e6796ce514b89c364ce6dee0a3a33188c3367a2b
Felix Victor <felix.victor.na@gmail.com>
2018-09-30 13:02:55 +0200

    journey wip
    Former-commit-id: 8206486f57656636fbd47c0adbb48521095b6d68 [formerly 32ef69f4908e593c275c40f85fea8897d17e26ac]
    Former-commit-id: 50a1d20f76adb1eec2195c744b80c25807248e72

---
+++ src/js/make-journey.js
@@ -0,0 +1,164 @@
+/**
+ * This file is part of na-map.
+ *
+ * @file      Make a journey.
+ * @module    make-journey
+ * @author    iB aka Felix Victor
+ * @copyright 2018
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+/* global d3 : false
+ */
+
+import "round-slider/src/roundslider";
+
+import {
+    compassDirections,
+    degreesToCompass,
+    formatInt,
+    formatFloat,
+    getOrdinal,
+    isEmpty,
+    capitalizeFirstLetter
+} from "./util";
+import { registerEvent } from "./analytics";
+import ShipCompare from "./ship-compare";
+import WoodCompare from "./wood-compare";
+
+/**
+ * Journey
+ */
+export default class Journey {
+    /**
+     * @param {Object} shipData - Ship data
+     * @param {Object} woodData - Wood data
+     */
+    constructor(shipData, woodData) {
+        this._shipData = shipData;
+        this._woodData = woodData;
+        this._baseId = "ship-journey";
+        this._woodId = "journey-wood";
+        this._setupListener();
+    }
+
+    /**
+     * Setup menu item listener
+     * @returns {void}
+     */
+    _setupListener() {
+        $("#journeyNavbar").on("shown.bs.dropdown", event => {
+            console.log(" journeyNavbar shown");
+            registerEvent("Menu", "Journey");
+            event.stopPropagation();
+                this._journeySelected();
+        });
+
+    }
+
+    /**
+     * Action when selected
+     * @returns {void}
+     */
+    _journeySelected() {
+        // $("#journeyMenu").dropdown("update");
+
+
+        this._injectInputs();
+        $("#journeyDropdown").dropdown();
+
+        this._shipCompare = new ShipCompare(this._shipData, this._woodData, this._baseId);
+        this._woodCompare = new WoodCompare(this._woodData, this._woodId);
+
+        console.log(this._shipCompare);
+        console.log(this._shipCompare.ships);
+        console.log(this._shipCompare.ships.Base);
+        console.log(this._shipCompare.ships.Base._shipData);
+    }
+
+    _injectInputs() {
+        /*
+			<form id="course" class="p-2">
+				<div class="form-group">
+					<p class="form-text">1. Set current in-game wind</p>
+					<div id="direction" class="rslider"></div>
+				</div>
+			</form>
+		*/
+        if (d3.select("#journeyMenu form").empty()) {
+            const form = d3
+                .select("#journeyMenu")
+                .append("form")
+                .attr("class", "p-2");
+            const slider = form.append("div").attr("class", "form-group");
+            slider
+                .append("p")
+                .attr("class", "form-text")
+                .text("1. Set current in-game wind");
+            slider
+                .append("div")
+                .attr("id", "course-wind-direction")
+                .attr("class", "rslider");
+
+            const select = form.append("div").attr("class", "form-group");
+            select
+                .append("p")
+                .attr("class", "form-text")
+                .text("2. Set ship");
+            const shipId = `${this._baseId}-Base-select`;
+            select.append("label").attr("for", shipId);
+            select
+                .append("select")
+                .attr("name", shipId)
+                .attr("id", shipId);
+
+            ["frame", "trim"].forEach(type => {
+                const woodId = `${this._woodId}-${type}-Base-select`;
+                select.append("label").attr("for", woodId);
+                select
+                    .append("select")
+                    .attr("name", woodId)
+                    .attr("id", woodId);
+            });
+        }
+    }
+
+    static _setupWindInput() {
+        // workaround from https://github.com/soundar24/roundSlider/issues/71
+        // eslint-disable-next-line func-names,no-underscore-dangle
+        const { _getTooltipPos } = $.fn.roundSlider.prototype;
+        // eslint-disable-next-line func-names,no-underscore-dangle
+        $.fn.roundSlider.prototype._getTooltipPos = function() {
+            if (!this.tooltip.is(":visible")) {
+                $("body").append(this.tooltip);
+            }
+            const pos = _getTooltipPos.call(this);
+            this.container.append(this.tooltip);
+            return pos;
+        };
+
+        window.tooltip = args => degreesToCompass(args.value);
+
+        $("#course-wind-direction").roundSlider({
+            sliderType: "default",
+            handleSize: "+1",
+            startAngle: 90,
+            width: 20,
+            radius: 110,
+            min: 0,
+            max: 359,
+            step: 360 / compassDirections.length,
+            editableTooltip: false,
+            tooltipFormat: "tooltip",
+            stop: "_getWindDirection",
+            create() {
+                this.control.css("display", "block");
+            }
+        });
+    }
+
+    static _getWindDirection(event) {
+        (this._currentWind = $("#course-wind-direction").roundSlider("option", "value")),
+            console.log(this._currentWind, event, event.type);
+    }
+}
