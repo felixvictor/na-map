2c4ac50968295b9da88b9469303afe8851759d4b
Felix Victor <felix.victor.na@gmail.com>
2018-10-03 14:14:33 +0200

    journey wip

--- src/js/make-journey.js
+++ src/js/make-journey.js
@@ -11,20 +11,15 @@
 /* global d3 : false
  */
 
+import moment from "moment";
+import "moment/locale/en-gb";
 import "round-slider/src/roundslider";
 
-import {
-    compassDirections,
-    degreesToCompass,
-    formatInt,
-    formatFloat,
-    getOrdinal,
-    isEmpty,
-    capitalizeFirstLetter
-} from "./util";
+import { compassDirections, degreesToCompass, rotationAngleInDegrees, formatF11 } from "./util";
 import { registerEvent } from "./analytics";
 import ShipCompare from "./ship-compare";
 import WoodCompare from "./wood-compare";
+import { convertInvCoordX, convertInvCoordY, getDistance, speedFactor } from "./common";
 
 /**
  * Journey
@@ -34,12 +29,38 @@ export default class Journey {
      * @param {Object} shipData - Ship data
      * @param {Object} woodData - Wood data
      */
-    constructor(shipData, woodData) {
+    constructor(shipData, woodData, fontSize) {
         this._shipData = shipData;
         this._woodData = woodData;
-        this._baseId = "ship-journey";
+        this._fontSize = fontSize;
+
+        this._bFirstCoord = true;
+        this._compassSize = 100;
+        this._line = d3
+            .line()
+            .x(d => d.x)
+            .y(d => d.y);
+        this._lineData = [];
+
+        this._minutesForFullCircle = 48;
+        this._fullCircle = 360;
+        this._degreesPerMinute = this._fullCircle / this._minutesForFullCircle;
+        this._degreesSegment = 15;
+        this._minOWSpeed = 2;
+        this._owSpeedFactor = 2;
+
+        this._totalDistance = 0;
+        this._totalMinutes = 0;
+        this._degreesCurrentWind = null;
+        this._currentShipId = null;
+
+        this._speedScale = d3.scaleLinear().domain(d3.range(0, this._fullCircle, this._degreesSegment));
+
+        this._setupSvg();
+        this._shipId = "ship-journey";
         this._woodId = "wood-journey";
-        this._setupListener();
+        this._selected = false;
+        // this._setupListener();
     }
 
     /**
@@ -48,6 +69,7 @@ export default class Journey {
      */
     _setupListener() {
         $("#journeyNavbar").on("shown.bs.dropdown", event => {
+            console.log("#journeyNavbar shown.bs.dropdown");
             registerEvent("Menu", "Journey");
             event.stopPropagation();
             this._journeySelected();
@@ -95,16 +117,39 @@ export default class Journey {
      * @returns {void}
      */
     _journeySelected() {
-        this._injectInputs();
-        this._setupWindInput();
+        if (!this._selected) {
+            console.log("_journeySelected Anfang");
+            this._selected = true;
 
-        this._shipCompare = new ShipCompare(this._shipData, this._woodData, this._baseId);
-        this._woodCompare = new WoodCompare(this._woodData, this._woodId);
+            this._injectInputs();
+            this._setupWindInput();
 
-        console.log(this._shipCompare);
-        console.log(this._shipCompare.ships);
-        console.log(this._shipCompare.ships.Base);
-        console.log(this._shipCompare.ships.Base._shipData);
+            /*
+            // Adapted https://github.com/bootstrapthemesco/bootstrap-4-multi-dropdown-navbar
+            $(".nav-item .dropdown-menu .bootstrap-select .dropdown-toggle").on("click", event => {
+                const $el = $(event.currentTarget);
+
+                $el.next(".dropdown-menu").toggleClass("show");
+                $el.parent("li").toggleClass("show");
+                $el.parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", event2 => {
+                    $(event2.currentTarget)
+                        .find("div.dropdown-menu.show")
+                        .removeClass("show");
+                });
+
+                return false;
+            });
+            */
+
+            this._shipCompare = new ShipCompare(this._shipData, this._woodData, this._shipId);
+            this._woodCompare = new WoodCompare(this._woodData, this._woodId);
+
+            console.log(this._shipCompare);
+            console.log(this._shipCompare.ships);
+            console.log(this._shipCompare.ships.Base);
+            console.log(this._shipCompare.ships.Base._shipData);
+            console.log("_journeySelected Ende");
+        }
     }
 
     _injectInputs() {
@@ -131,20 +176,207 @@ export default class Journey {
                 .text("2. Set ship");
 
             const div1 = select.append("div").attr("class", "dropdown-item");
-            const shipId = `${this._baseId}-Base-select`;
-            div1.append("label").attr("for", shipId);
-            div1.append("select")
+            const shipId = `${this._shipId}-Base-select`;
+            div1.append("label")
+                .append("select")
                 .attr("name", shipId)
                 .attr("id", shipId);
 
             ["frame", "trim"].forEach(type => {
                 const div2 = select.append("div").attr("class", "dropdown-item");
                 const woodId = `${this._woodId}-${type}-Base-select`;
-                div2.append("label").attr("for", woodId);
-                div2.append("select")
+                div2.append("label")
+                    .append("select")
                     .attr("name", woodId)
                     .attr("id", woodId);
             });
         }
+    }
+
+    /* private */
+    _setupSvg() {
+        this._g = d3
+            .select("#na-svg")
+            .append("g")
+            .classed("coord", true);
+
+        d3.select("#na-svg defs")
+            .append("marker")
+            .attr("id", "course-arrow")
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 8)
+            .attr("refY", 0)
+            .attr("markerWidth", 5)
+            .attr("markerHeight", 5)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("d", "M0,-5L10,0L0,5")
+            .attr("class", "course-head");
+    }
+
+    _printCompass() {
+        const pos = this._lineData.length - 1,
+            x = this._lineData[pos].x,
+            y = this._lineData[pos].y;
+        this._g
+            .append("image")
+            .classed("compass", true)
+            .attr("x", x)
+            .attr("y", y)
+            .attr("transform", `translate(${-this._compassSize / 2},${-this._compassSize / 2})`)
+            .attr("height", this._compassSize)
+            .attr("width", this._compassSize)
+            .attr("xlink:href", "icons/compass.svg");
+        this.gCompass = this._g.append("path");
+    }
+
+    _getSpeedAtDegrees(degrees) {
+        return Math.max(this._speedScale(degrees), this._minOWSpeed);
+    }
+
+    _calculateDistanceForSection(degreesCourse, degreesCurrentWind) {
+        const degreesForSpeedCalc = (this._fullCircle - degreesCourse + degreesCurrentWind) % this._fullCircle,
+            speedCurrentSection = this._getSpeedAtDegrees(degreesForSpeedCalc) * this._owSpeedFactor,
+            distanceCurrentSection = speedCurrentSection * speedFactor;
+        /*
+        console.log(
+            { degreesCourse },
+            { degreesCurrentWind },
+            { degreesForSpeedCalc },
+            { speedCurrentSection },
+            { distanceCurrentSection }
+        );
+*/
+        return distanceCurrentSection;
+    }
+
+    _getStartWind() {
+        return 27;
+    }
+
+    _getCurrentShipId() {
+        let id = this._shipDefualtId;
+        if (typeof this._shipCompare.ships.Base._shipData !== "undefined") {
+            id = this._shipCompare.ships.Base._shipData.id;
+        }
+    }
+
+    _setShipSpeed() {
+        const id = this._getCurrentShipId(),
+            speedDegrees = this._shipCompare.ships.Base._shipData
+                .filter(ship => ship.id === id)
+                .map(ship => ship.speedDegrees)[0];
+        this._speedScale.range(speedDegrees);
+    }
+
+    _calculateMinutesForCourse(degreesCourse, degreesStartWind, distanceTotal) {
+        let distanceRemaining = distanceTotal,
+            degreesCurrentWind = degreesStartWind,
+            totalMinutes = 0;
+        while (distanceRemaining > 0) {
+            const distanceCurrentSection = this._calculateDistanceForSection(degreesCourse, degreesCurrentWind);
+            if (distanceRemaining > distanceCurrentSection) {
+                distanceRemaining -= distanceCurrentSection;
+                totalMinutes += 1;
+            } else {
+                totalMinutes += distanceRemaining / distanceCurrentSection;
+                distanceRemaining = 0;
+            }
+            degreesCurrentWind = (this._fullCircle + degreesCurrentWind - this._degreesPerMinute) % this._fullCircle;
+
+            // console.log({ distanceCurrentSection }, { totalMinutes });
+        }
+        this._degreesCurrentWind = degreesCurrentWind;
+
+        return totalMinutes;
+    }
+
+    _printLine() {
+        this._setShipSpeed();
+        const pos1 = this._lineData.length - 1,
+            pos2 = this._lineData.length - 2,
+            pt1 = this._lineData[pos1],
+            pt2 = this._lineData[pos2],
+            degreesCourse = rotationAngleInDegrees(pt1, pt2),
+            degreesStartWind = this._degreesCurrentWind || this._getStartWind(),
+            distance = getDistance(pt1, pt2),
+            compass = degreesToCompass(degreesCourse),
+            minutes = this._calculateMinutesForCourse(degreesCourse, degreesStartWind, distance * 1000);
+
+        this._totalDistance += distance;
+        this._totalMinutes += minutes;
+        const duration = moment.duration(minutes, "minutes").humanize(true),
+            totalDuration = moment.duration(this._totalMinutes, "minutes").humanize(true),
+            textDirection = `${compass} (${Math.round(degreesCourse)}°) \u2606 F11: ${formatF11(
+                convertInvCoordX(pt1.x, pt1.y)
+            )}\u202f/\u202f${formatF11(convertInvCoordY(pt1.x, pt1.y))}`;
+        let textDistance = `${Math.round(distance)}k ${duration}`;
+        const textLines = 2;
+        if (this._lineData.length > 2) {
+            textDistance += ` \u2606 total ${Math.round(this._totalDistance)}k ${totalDuration}`;
+        }
+
+        this.gCompass
+            .datum(this._lineData)
+
+            .attr("marker-end", "url(#course-arrow)")
+            .attr("d", this._line);
+
+        const svg = this._g
+            .append("svg")
+            .attr("x", pt1.x)
+            .attr("y", pt1.y);
+        const textBackgroundBox = svg.append("rect");
+        const textDirectionBox = svg
+            .append("text")
+            .attr("x", "10%")
+            .attr("y", "33%")
+            .text(textDirection);
+
+        const textDistanceBox = svg
+            .append("text")
+            .attr("x", "10%")
+            .attr("y", "66%")
+            .text(textDistance);
+
+        const bbTextDirectionBox = textDirectionBox.node().getBBox(),
+            bbTextDistanceBox = textDistanceBox.node().getBBox();
+
+        const height = (bbTextDirectionBox.height + this._fontSize) * textLines * 1.1,
+            width = (Math.max(bbTextDirectionBox.width, bbTextDistanceBox.width) + this._fontSize) * 1.1;
+        textBackgroundBox
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("height", height)
+            .attr("width", width);
+        svg.attr("height", height).attr("width", width);
+    }
+
+    /* public */
+    plotCourse(x, y) {
+        if (this._bFirstCoord) {
+            this.clearMap();
+        }
+        this._lineData.push({ x, y });
+        if (this._bFirstCoord) {
+            this._printCompass();
+            this._bFirstCoord = !this._bFirstCoord;
+        } else {
+            this._printLine();
+        }
+    }
+
+    transform(transform) {
+        this._g.attr("transform", transform);
+    }
+
+    clearMap() {
+        this._bFirstCoord = true;
+        this._totalDistance = 0;
+        if (typeof this._lineData !== "undefined") {
+            this._lineData.splice(0, this._lineData.length);
+        }
+
+        this._g.selectAll("*").remove();
     }
 }
