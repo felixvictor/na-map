04290e1d6930e770bcaedbcaab7ec619d09efca4
Felix Victor <felix.victor.na@gmail.com>
2018-10-09 17:56:58 +0200

    nested dropdowns

--- src/js/common.js
+++ src/js/common.js
@@ -118,16 +118,11 @@ export function insertBaseModal(id, title, large = true) {
  */
 export function initMultiDropdownNavbar(id) {
     $(`#${id} .dropdown-menu .bootstrap-select .dropdown-toggle`).on("click", event => {
-        console.log("+++++++++++++++ click '.nav-item .dropdown-menu .bootstrap-select .dropdown-toggle'");
         const el$ = $(event.currentTarget);
 
         el$.next(".dropdown-menu").toggleClass("show");
         el$.parent("li").toggleClass("show");
         el$.parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", event2 => {
-            console.log(
-                "********** hidden.bs.dropdown 'li.nav-item.dropdown.show'",
-                $(event2.currentTarget).find(".dropdown.bootstrap-select > .dropdown-menu.show")
-            );
             $(event2.currentTarget)
                 .find(".dropdown-menu.show")
                 .not(".inner")
@@ -138,7 +133,6 @@ export function initMultiDropdownNavbar(id) {
     });
 
     $(`#${id} div.dropdown.bootstrap-select`).on("hidden", event => {
-        console.log("#################### hidden.bs.dropdown '.dropdown'");
         // hide any open menus when parent closes
         $(event.currentTarget)
             .find(".dropdown-menu.show")
