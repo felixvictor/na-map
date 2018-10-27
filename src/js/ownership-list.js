21fdfc266767bc85c5bbb05cb7baa92e2961017f
Felix Victor <felix.victor.na@gmail.com>
2018-10-27 23:00:55 +0200

    port ownership timeline wip
    Former-commit-id: f3887ed86a8eb14b83fe03a90faa29050779b6e0

--- src/js/ownership-list.js
+++ src/js/ownership-list.js
@@ -8,13 +8,21 @@
  * @license   http://www.gnu.org/licenses/gpl.html
  */
 
-import { scaleOrdinal as d3ScaleOrdinal } from "d3-scale";
+import { areaLabel as d3AreaLabel } from "d3-area-label";
+import { extent as d3Extent, max as d3Max, min as d3Min } from "d3-array";
+import { axisBottom as d3AxisBottom } from "d3-axis";
+import { scaleLinear as d3ScaleLinear, scaleOrdinal as d3ScaleOrdinal, scaleTime as d3ScaleTime } from "d3-scale";
 import { select as d3Select } from "d3-selection";
-import { getContentRect } from "resize-observer-polyfill/src/utils/geometry";
+import {
+    area as d3Area,
+    curveBasis as d3CurveBasis,
+    stack as d3Stack,
+    stackOffsetWiggle as d3StackOffsetWiggle
+} from "d3-shape";
 import TimelinesChart from "timelines-chart";
 
-import { insertBaseModal } from "./common";
 import { registerEvent } from "./analytics";
+import { insertBaseModal, nations } from "./common";
 
 // https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
 // eslint-disable-next-line no-extend-native,func-names
@@ -29,14 +37,32 @@ String.prototype.replaceAll = function(search, replacement) {
 export default class OwnershipList {
     /**
      * @param {object} ownershipData - Port ownership data over time
+     * @param {object} nationData - Nation data over time
      */
-    constructor(ownershipData) {
+    constructor(ownershipData, nationData) {
         this._ownershipData = ownershipData;
+        this._nationData = nationData;
 
         this._baseName = "Port ownership";
         this._baseId = "ownership-list";
         this._buttonId = `button-${this._baseId}`;
         this._modalId = `modal-${this._baseId}`;
+
+        // http://tools.medialab.sciences-po.fr/iwanthue/
+        this._colourScale = d3ScaleOrdinal().range([
+            "#72823a",
+            "#825fc8",
+            "#78b642",
+            "#cd47a3",
+            "#50b187",
+            "#d34253",
+            "#628bcc",
+            "#cb9f3d",
+            "#cc88c9",
+            "#ca5b2b",
+            "#b55576",
+            "#c27b58"
+        ]);
 
         this._setupListener();
     }
@@ -62,10 +88,11 @@ export default class OwnershipList {
     _injectModal() {
         insertBaseModal(this._modalId, this._baseName);
 
-        this._div = d3Select(`#${this._modalId} .modal-body`)
+        const div = d3Select(`#${this._modalId} .modal-body`)
             .append("div")
-            .attr("id", `${this._baseId}`)
-            .append("div");
+            .attr("id", `${this._baseId}`);
+        this._div = div.append("div");
+        this._svg = div.append("svg").attr("class", "area");
     }
 
     /**
@@ -95,11 +122,21 @@ export default class OwnershipList {
             .on("shown.bs.modal", () => {
                 // Inject chart after modal is shown to calculate modal width
                 if (emptyModal) {
-                    this._injectChart();
+                    // this._injectChart();
+                    this._injectArea();
                     emptyModal = false;
                 }
             })
             .modal("show");
+    }
+
+    static getHeight() {
+        // eslint-disable-next-line no-restricted-globals
+        return Math.floor(top.innerHeight * 0.75);
+    }
+
+    _getWidth() {
+        return Math.floor(document.getElementById(this._baseId).offsetWidth);
     }
 
     /**
@@ -108,26 +145,79 @@ export default class OwnershipList {
      * @private
      */
     _injectChart() {
-        // http://tools.medialab.sciences-po.fr/iwanthue/
-        const colourScale = d3ScaleOrdinal().range([
-                "#72823a",
-                "#825fc8",
-                "#78b642",
-                "#cd47a3",
-                "#50b187",
-                "#d34253",
-                "#628bcc",
-                "#cb9f3d",
-                "#cc88c9",
-                "#ca5b2b",
-                "#b55576",
-                "#c27b58"
-            ]),
-            chart = TimelinesChart()
-                .data(this._ownershipData)
-                .timeFormat("%-d %B %Y")
-                .zQualitative(true)
-                .zColorScale(colourScale)
-                .width(document.getElementById(this._baseId).offsetWidth)(this._div.node());
+        const chart = TimelinesChart()
+            .data(this._ownershipData)
+            .timeFormat("%-d %B %Y")
+            .zQualitative(true)
+            .zColorScale(this._colourScale)
+            .width(this._getWidth())(this._div.node());
+    }
+
+    _injectArea() {
+        const xValue = d => new Date(d.date);
+
+        const width = this._getWidth(),
+            // eslint-disable-next-line no-restricted-globals
+            height = OwnershipList.getHeight(),
+            margin = { top: 0, right: 32, bottom: 32, left: 32 };
+
+        const stack = d3Stack().offset(d3StackOffsetWiggle),
+            xScale = d3ScaleLinear(),
+            yScale = d3ScaleLinear(),
+            keys = nations.filter(nation => nation.id !== 9).map(nation => nation.short),
+            labelNames = new Map(nations.filter(nation => nation.id !== 9).map(nation => [nation.short, nation.name])),
+            data = this._nationData;
+
+        data.keys = keys;
+
+        const x = d3ScaleTime()
+                .domain(d3Extent(data, d => xValue(d)))
+                .range([margin.left, width - margin.right]),
+            xAxis = g =>
+                g.attr("transform", `translate(0,${height - margin.bottom})`).call(
+                    d3AxisBottom(x)
+                        .ticks(width / 80)
+                        .tickSizeOuter(0)
+                );
+
+        const area = d3Area()
+            .x(d => xScale(xValue(d.data)))
+            .y0(d => yScale(d[0]))
+            .y1(d => yScale(d[1]))
+            .curve(d3CurveBasis);
+
+        const render = data => {
+            stack.keys(data.keys);
+            this._colourScale.domain(data.keys);
+            const stacked = stack(data);
+
+            xScale.domain(d3Extent(data, d => xValue(d))).range([margin.left, width - margin.right]);
+
+            yScale
+                .domain([d3Min(stacked[0], d => d[0]), d3Max(stacked[stacked.length - 1], d => d[1])])
+                .range([height - margin.bottom, 0]);
+
+            const paths = this._svg.selectAll("path").data(stacked);
+            paths
+                .enter()
+                .append("path")
+                .merge(paths)
+                .attr("fill", d => this._colourScale(d.key))
+                .attr("stroke", d => this._colourScale(d.key))
+                .attr("d", area);
+
+            const labels = this._svg.selectAll(".area-label").data(stacked);
+            labels
+                .enter()
+                .append("text")
+                .attr("class", "area-label")
+                .merge(labels)
+                .text(d => labelNames.get(d.key))
+                .attr("transform", d3AreaLabel(area));
+        };
+
+        this._svg.attr("width", width).attr("height", height);
+        render(data);
+        this._svg.append("g").call(xAxis);
     }
 }
