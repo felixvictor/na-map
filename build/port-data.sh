cb4704cb003dfb8ce53f0e54cfc59af3189e679d
Felix Victor <felix.victor.na@gmail.com>
2018-11-08 10:36:48 +0100

    port-data: wrong double brackets removed
    Former-commit-id: 7afee608281cbecd2ac8f8b04896f8ca9bd399bd

--- build/port-data.sh
+++ build/port-data.sh
@@ -23,7 +23,7 @@ API_VARS=(ItemTemplates Ports Shops)
 SERVER_MAINTENANCE_HOUR=10
 HEADER_DATE=$(LC_TIME="en" date -u +"%a, %d %b %Y 10:00:00 GMT" -d "+1 day")
 # Set server date
-if [[ "$(date -u '+%H')" -lt "${SERVER_MAINTENANCE_HOUR}" ]]; then
+if [ "$(date -u '+%H')" -lt "${SERVER_MAINTENANCE_HOUR}" ]; then
     DATE=$(date -u '+%Y-%m-%d' --date "-1 day")
     LAST_DATE=$(date -u '+%Y-%m-%d' --date "-2 day")
 else
@@ -55,7 +55,7 @@ function pull_all () {
 
 function on_exit () {
     # If git push fails, git pull first
-    if [[ "${LAST_FUNCTION}" == "push_data" ]]; then
+    if [ "${LAST_FUNCTION}" == "push_data" ]; then
         pull_all
         git_push_all
         exit 0
@@ -109,7 +109,7 @@ function get_git_update () {
     LOCAL=$(git rev-parse @)
     BASE=$(git merge-base @ "@{u}")
 
-    if [[ "${LOCAL}" == "${BASE}" ]]; then
+    if [ "${LOCAL}" == "${BASE}" ]; then
         git pull
     fi
 }
@@ -141,7 +141,7 @@ function get_port_data () {
     mkdir -p "${API_DIR}"
     if test_for_update "${API_BASE_FILE}"; then
         for JSON in "${API_DIR}"/*.json; do
-            if [[ "${JSON}" != "${TWEETS_JSON}" ]]; then
+            if [ "${JSON}" != "${TWEETS_JSON}" ]; then
                 ${XZ} -9ef "${JSON}"
             fi
         done
@@ -176,7 +176,7 @@ function get_port_data () {
         ${NODE} build/convert-buildings.mjs "${API_BASE_FILE}-${SERVER_NAMES[0]}" "${BUILDING_FILE}" "${DATE}"
         ${NODE} build/convert-loot.mjs "${API_BASE_FILE}-${SERVER_NAMES[0]}" "${LOOT_FILE}" "${DATE}"
         ${NODE} build/convert-recipes.mjs "${API_BASE_FILE}-${SERVER_NAMES[0]}" "${RECIPE_FILE}" "${DATE}"
-        if [[ "${SCRIPT_RUN_TYPE}" == "update" ]]; then
+        if [ "${SCRIPT_RUN_TYPE}" == "update" ]; then
             ${NODE} build/convert-ownership.mjs "${API_DIR}" "${OWNERSHIP_FILE}" "${NATION_FILE}"
         fi
 
@@ -259,7 +259,7 @@ function push_data () {
     git add --ignore-errors .
     if [[ -n $(git status -s) ]]; then
         GIT_MESSAGE=""
-        if [[ "${SCRIPT_RUN_TYPE}" == "update" ]]; then
+        if [ "${SCRIPT_RUN_TYPE}" == "update" ]; then
             GIT_MESSAGE+="squash! "
             touch "${LAST_UPDATE_FILE}"
         fi
@@ -280,7 +280,7 @@ function update_data () {
     fi
     LAST_UPDATE=$(date --reference="${LAST_UPDATE_FILE}" +%Y-%m-%d)
     # Test if already updated today
-    if [[ "${LAST_UPDATE}" != "${DATE}" ]]; then
+    if [ "${LAST_UPDATE}" != "${DATE}" ]; then
         update_yarn
         get_git_update
         # Test if new API data available
