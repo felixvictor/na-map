2047fb10306ceb315518cf04f8f09b1f84b07236
Felix Victor <felix.victor.na@gmail.com>
2018-12-08 17:39:13 +0100

    lazy loading wip

---
+++ src/js/game-tools/index.js
@@ -0,0 +1,15 @@
+/**
+ * This file is part of na-map.
+ *
+ * @file      Game tools main file.
+ * @module    game-tools/index
+ * @author    iB aka Felix Victor
+ * @copyright 2017, 2018
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+function init() {
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { init };
