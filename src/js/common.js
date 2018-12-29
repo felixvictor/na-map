de3a5cb1169cbdaff0dbe3e62dbe824276f19db0
Felix Victor <felix.victor.na@gmail.com>
2018-12-29 18:05:29 +0100

    colours: vars instead hex values

--- src/js/common.js
+++ src/js/common.js
@@ -160,3 +160,12 @@ export const speedConstA = 0.074465523706782;
 export const speedConstB = 0.00272175949231;
 
 export const circleRadiusFactor = 5;
+
+export const colourGray200 = CGRAY200;
+export const colourGray500 = CGRAY500;
+export const colourGray700 = CGRAY700;
+export const colourGreen = CGREEN;
+export const colourGreenDark = CGREENDARK;
+export const colourRed = CRED;
+export const colourRedDark = CREDDARK;
+export const colourWhite = CWHITE;
