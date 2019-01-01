3a48d93ea0c4c97bfb2bf746befca1ef0a16c36f
Felix Victor <felix.victor.na@gmail.com>
2019-01-01 21:09:56 +0100

    ship blueprint
    Former-commit-id: 718e912f7ec2b7ad61f241372ca2aa7b00c567ba [formerly 13e84c02bea7bcd961b0958c350d9c06a540893c]
    Former-commit-id: 27cf65b067e48fb1b40ea242086cc74752d3c588

--- src/js/game-tools/list-ship-blueprints.js
+++ src/js/game-tools/list-ship-blueprints.js
@@ -11,12 +11,11 @@
 import { select as d3Select } from "d3-selection";
 import { formatInt, getOrdinal } from "../util";
 import { registerEvent } from "../analytics";
-import { getCurrencyAmount, insertBaseModal } from "../common";
+import { insertBaseModal } from "../common";
 
 export default class ListShipBlueprints {
     constructor(blueprintData) {
-        this._blueprintData = blueprintData.shipBlueprints;
-        this._ratioData = blueprintData.ratios;
+        this._blueprintData = blueprintData;
 
         this._baseName = "List ship blueprint";
         this._baseId = "ship-blueprint-list";
@@ -90,19 +89,25 @@ export default class ListShipBlueprints {
         return this._blueprintData.find(blueprint => blueprint.name === selectedBlueprint);
     }
 
+    // noinspection JSMethodCanBeStatic
     _getShipText(currentBlueprint) {
         let text = "";
 
         text += '<table class="table table-sm"><tbody>';
 
         text += `<tr><td>Ship rate</td><td>${getOrdinal(currentBlueprint.ship.rate)}</td></tr>`;
-        text += `<tr><td>Ship mass</td><td>${formatInt(currentBlueprint.ship.shipMass)}</td></tr>`;
-        text += `<tr><td>Maximum crew</td><td>${formatInt(currentBlueprint.ship.maxCrew)}</td></tr>`;
 
-        text += `<tr><td>Shipyard level</td><td>${formatInt(currentBlueprint.shipyardLevel)}</td></tr>`;
         text += `<tr><td>Craft level</td><td>${formatInt(currentBlueprint.craftLevel)}</td></tr>`;
+        text += `<tr><td>Shipyard level</td><td>${formatInt(currentBlueprint.shipyardLevel)}</td></tr>`;
         text += `<tr><td>Labour hours</td><td>${formatInt(currentBlueprint.labourHours)}</td></tr>`;
         text += `<tr><td>Craft experience gained</td><td>${formatInt(currentBlueprint.craftXP)}</td></tr>`;
+        if (currentBlueprint.doubloons) {
+            text += `<tr><td>Doubloons</td><td>${formatInt(currentBlueprint.doubloons)}</td></tr>`;
+        }
+        text += `<tr><td>Provisions</td><td>${formatInt(currentBlueprint.provisions)}</td></tr>`;
+        if (currentBlueprint.permit) {
+            text += `<tr><td>Permit</td><td>${formatInt(currentBlueprint.permit)}</td></tr>`;
+        }
 
         text += "</tbody></table>";
         return text;
@@ -111,23 +116,25 @@ export default class ListShipBlueprints {
     _getResourcesText(currentBlueprint) {
         let text = "";
 
-        text += '<table class="table table-sm"><thead>';
-        text += "<tr><th>Level</th><th>Level build materials</th></tr>";
-        text += "</thead><tbody>";
+        text += '<table class="table table-sm"><tbody>';
+        currentBlueprint.resources.forEach(resource => {
+            text += `<tr><td>${resource.name}</td><td>${formatInt(resource.amount)}</td></tr>`;
+        });
         text += "</tbody></table>";
         return text;
     }
 
-    _getRequirementText(currentBlueprint) {
+    _getPlankingText(currentBlueprint) {
         let text = "";
 
-        text += '<table class="table table-sm"><thead>';
-
-        text += "<tr><th>Requirement</th><th>Amount</th></tr>";
-        text += "</thead><tbody>";
-
-        currentBlueprint.resources.forEach(resource => {
-            text += `<tr><td>${resource.name}</td><td>${formatInt(material.amount)}</td></tr>;
+        text += '<table class="table table-sm"><tbody>';
+        currentBlueprint.frames.forEach(frame => {
+            text += `<tr><td>${frame.name}</td><td>${formatInt(frame.amount)}</td></tr>`;
+        });
+        currentBlueprint.trims.forEach(trim => {
+            text += `<tr><td>${trim.name}</td><td>+ ${formatInt(trim.amount)} ${
+                trim.name === "Planking" ? "logs" : "Hemp"
+            }</td></tr>`;
         });
         text += "</tbody></table>";
 
@@ -151,13 +158,13 @@ export default class ListShipBlueprints {
         text += "</div></div>";
 
         text += '<div class="card col-4"><div class="card-header">Resources</div>';
-        text += '<div class="card-body px-0">';
+        text += '<div class="card-body">';
         text += this._getResourcesText(currentBlueprint);
         text += "</div></div>";
 
-        text += '<div class="card col-4"><div class="card-header">Requirements</div>';
-        text += '<div class="card-body px-0">';
-        text += this._getRequirementText(currentBlueprint);
+        text += '<div class="card col-4"><div class="card-header">Woods</div>';
+        text += '<div class="card-body">';
+        text += this._getPlankingText(currentBlueprint);
         text += "</div></div>";
 
         text += "</div>";
