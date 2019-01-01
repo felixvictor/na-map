a28eb3ff78dc1401d13db72e61e418800b5a7934
Felix Victor <felix.victor.na@gmail.com>
2019-01-01 02:50:02 +0100

    convert ship recipes
    Former-commit-id: 0e97db440781df18d5c13430b635108d987f8a4d [formerly f855d41a1c0df23c49a8245e31950867b1ee4484]
    Former-commit-id: 4d2e7d90128e40a3dbbb817a7c166a4f0bade7d2

--- build/convert-ship-recipes.mjs
+++ build/convert-ship-recipes.mjs
@@ -14,7 +14,7 @@ const inBaseFilename = process.argv[2],
     outFilename = process.argv[3],
     date = process.argv[4];
 
-const APIItems = readJson(`${inBaseFilename}-ItemTemplates-${date}.json`);
+const apiItems = readJson(`${inBaseFilename}-ItemTemplates-${date}.json`);
 
 // https://stackoverflow.com/questions/1144783/how-to-replace-all-occurrences-of-a-string-in-javascript
 // eslint-disable-next-line no-extend-native,func-names
@@ -24,79 +24,78 @@ String.prototype.replaceAll = function(search, replacement) {
 };
 
 const convertShipRecipes = () => {
+    const getItemNames = () => new Map(apiItems.map(item => [item.Id, item.Name.replaceAll("'", "’")]));
+    const getShipMass = id => apiItems.find(apiItem => id === apiItem.Id).ShipMass;
+
     const data = {};
-    const itemNames = new Map();
+    const itemNames = getItemNames();
     data.shipRecipes = [];
-
-    const getItemNames = () => {
-        APIItems.forEach(item => {
-            itemNames.set(item.Id, item.Name.replaceAll("'", "’"));
-        });
-    };
-
-    getItemNames();
 
     let permit = { needed: false };
     let doubloons = { needed: 0 };
-    APIItems.filter(item => !item.NotUsed && item.ItemType === "RecipeShip").forEach(APIrecipe => {
-        const recipe = {
-            id: APIrecipe.Id,
-            name: APIrecipe.Name.replaceAll("'", "’"),
-            woods: APIrecipe.WoodTypeDescs.map(wood => ({
-                name: itemNames.get(wood.Requirements[0].Template).replace(" Log", ""),
-                amount: wood.Requirements[0].Amount
-            })),
-            resources: APIrecipe.FullRequirements.map((requirement, i) => {
-                if (itemNames.get(requirement.Template).endsWith(" Permit")) {
-                    permit = { needed: true, index: i };
-                }
-                if (itemNames.get(requirement.Template) === "Doubloons") {
-                    doubloons = { needed: requirement.Amount, index: i };
-                }
-                return {
-                    name: itemNames.get(requirement.Template),
-                    amount: requirement.Amount
-                };
-            }),
-            // gold: APIrecipe.GoldRequirements,
-            ship: {
-                id: APIrecipe.Results[0].Template,
-                name: itemNames.get(APIrecipe.Results[0].Template)
-            },
-            // qualities: APIrecipe.Qualities,
-            shipyardLevel: APIrecipe.BuildingRequirements[0].Level + 1,
-            craftLevel: APIrecipe.RequiresLevel,
-            craftXP: APIrecipe.GivesXP,
-            labourHours: APIrecipe.LaborPrice
-        };
-        if (permit.needed) {
-            recipe.permit = true;
-            // Remove permit from resources
-            recipe.resources.splice(permit.index, 1);
-        } else {
-            recipe.permit = false;
-        }
-        if (doubloons.needed) {
-            recipe.doubloons = doubloons.needed;
-            // Remove permit from resources
-            recipe.resources.splice(doubloons.index - (permit.needed === true ? 1 : 0), 1);
-        } else {
-            recipe.doubloons = doubloons.needed;
-        }
-        data.shipRecipes.push(recipe);
-        permit = { needed: false };
-        doubloons = { needed: 0 };
-    });
+    data.shipRecipes = apiItems
+        .filter(apiItem => !apiItem.NotUsed && apiItem.ItemType === "RecipeShip")
+        .map(apiRecipe => {
+            const shipMass = getShipMass(apiRecipe.Results[0].Template);
+            const recipe = {
+                id: apiRecipe.Id,
+                name: apiRecipe.Name.replaceAll("'", "’"),
+                frames: apiRecipe.WoodTypeDescs.map(wood => ({
+                    name: itemNames.get(wood.Requirements[0].Template).replace(" Log", ""),
+                    amount: wood.Requirements[0].Amount
+                })),
+                trims: { planking: Math.round(shipMass * 0.13), hemp: Math.round(shipMass * 0.025) },
+                resources: apiRecipe.FullRequirements.map((requirement, i) => {
+                    if (itemNames.get(requirement.Template).endsWith(" Permit")) {
+                        permit = { needed: true, index: i };
+                    }
+                    if (itemNames.get(requirement.Template) === "Doubloons") {
+                        doubloons = { needed: requirement.Amount, index: i };
+                    }
+                    return {
+                        name: itemNames.get(requirement.Template),
+                        amount: requirement.Amount
+                    };
+                }),
+                // gold: apiRecipe.GoldRequirements,
+                ship: {
+                    id: apiRecipe.Results[0].Template,
+                    name: itemNames.get(apiRecipe.Results[0].Template)
+                },
+                // qualities: apiRecipe.Qualities,
+                shipyardLevel: apiRecipe.BuildingRequirements[0].Level + 1,
+                craftLevel: apiRecipe.RequiresLevel,
+                craftXP: apiRecipe.GivesXP,
+                labourHours: apiRecipe.LaborPrice
+            };
+            if (permit.needed) {
+                recipe.permit = true;
+                // Remove permit from resources
+                recipe.resources.splice(permit.index, 1);
+            } else {
+                recipe.permit = false;
+            }
+            if (doubloons.needed) {
+                recipe.doubloons = doubloons.needed;
+                // Remove permit from resources
+                recipe.resources.splice(doubloons.index - (permit.needed === true ? 1 : 0), 1);
+            } else {
+                recipe.doubloons = doubloons.needed;
+            }
+            permit = { needed: false };
+            doubloons = { needed: 0 };
 
-    data.shipRecipes.sort((a, b) => {
-        if (a.name < b.name) {
-            return -1;
-        }
-        if (a.name > b.name) {
-            return 1;
-        }
-        return 0;
-    });
+            return recipe;
+        })
+        .sort((a, b) => {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        });
 
     saveJson(outFilename, data);
 };
