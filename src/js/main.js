d197b77fc6bbdf03e722af3a27b84332594e7b74
Felix Victor <felix.victor.na@gmail.com>
2019-01-06 18:17:17 +0100

    debug getServername
    Former-commit-id: 76bd40274ec39ea23f71cbf713a2fe3eda9197bb [formerly cb13432ee1d31dd9c90e7dead44a35f7f0036054]
    Former-commit-id: f8f21557d29037ba7f35164b459c8496b3b6eef8

--- src/js/main.js
+++ src/js/main.js
@@ -57,7 +57,10 @@ function main() {
     function getServerName() {
         // Use default value if cookie is not stored
         const r = Cookies.get(serverNameCookieName) || serverNameDefault;
-
+        console.log("getServerName");
+        console.log({ r }, { serverNameCookieName }, Cookies.get(serverNameCookieName), { serverNameDefault });
+        console.log(document.getElementById("navbarSupportedContent"));
+        console.log(document.getElementById(`server-name-${r}`));
         document.getElementById(`server-name-${r}`).checked = true;
 
         return r;
