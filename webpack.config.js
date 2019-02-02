d49b34d8169ae5d1acb79788029ac20d7081053b
Felix Victor <felix.victor.na@gmail.com>
2019-02-02 15:06:19 +0100

    webpack preload

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const // { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer"),
     CleanWebpackPlugin = require("clean-webpack-plugin"),
     CopyPlugin = require("copy-webpack-plugin"),
     HtmlPlugin = require("html-webpack-plugin"),
-    MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+    ExtractCssChunks = require("extract-css-chunks-webpack-plugin"),
     PreloadWebpackPlugin = require("preload-webpack-plugin"),
     SitemapPlugin = require("sitemap-webpack-plugin").default,
     SriPlugin = require("webpack-subresource-integrity"),
@@ -271,7 +271,7 @@ const config = {
         new CleanWebpackPlugin(outputPath, {
             verbose: false
         }),
-        new MiniCssExtractPlugin({ filename: isProd ? "[name].[contenthash].css" : "[name].css" }),
+        new ExtractCssChunks({ filename: isProd ? "[name].[contenthash].css" : "[name].css", orderWarning: true }),
         new webpack.DefinePlugin({
             CPRIMARY300: JSON.stringify(primary300),
             CGREEN: JSON.stringify(colourGreen),
@@ -316,8 +316,8 @@ const config = {
         ]),
         new HtmlPlugin(htmlOpt),
         new PreloadWebpackPlugin({
-            include: "allAssets",
-            fileWhitelist: [/\.woff2$/]
+            rel: "preload",
+            include: "initial"
         }),
         new SitemapPlugin(target, sitemapPaths, { skipGzip: false }),
         new SriPlugin({
@@ -325,7 +325,8 @@ const config = {
             enabled: isProd
         }),
         new WebpackDeepScopeAnalysisPlugin(),
-        new WebpackPwaManifest(manifestOpt)
+        new WebpackPwaManifest(manifestOpt),
+        new webpack.HashedModuleIdsPlugin()
     ],
 
     stats: {
@@ -356,7 +357,7 @@ const config = {
                 test: /\.scss$/,
                 include: path.resolve(__dirname, "src/scss"),
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    ExtractCssChunks.loader,
                     {
                         loader: "css-loader",
                         options: cssOpt
@@ -374,7 +375,7 @@ const config = {
             {
                 test: /\.css$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    ExtractCssChunks.loader,
                     {
                         loader: "css-loader",
                         options: cssOpt
