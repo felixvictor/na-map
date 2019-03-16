7538b95b8354c136155910780f4cd91bcb848134
Felix Victor <felix.victor.na@gmail.com>
2019-03-16 11:52:48 +0100

    servers as imported data
    Former-commit-id: a7137e85212f931df332f37add7e8c2afab09697 [formerly f69df1c0cf88c1d0b9626ada815da793368ade8b]
    Former-commit-id: 5e3d7a1abc996b394e7b4d8e6adcc9f9bd65e32d

---
+++ src/js/servers.js
@@ -0,0 +1,13 @@
+/**
+ * This file is part of na-map.
+ *
+ * @file      Game server data.
+ * @module    servers
+ * @author    iB aka Felix Victor
+ * @copyright 2019
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+const servers = [{ id: "eu1", name: "War", type: "PVP" }, { id: "eu2", name: "Peace", type: "PVE" }];
+
+module.exports = { servers };
