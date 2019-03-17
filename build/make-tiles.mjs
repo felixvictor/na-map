7827893bd6fb917333e41d76771b9510d6fd16c3
Felix Victor <felix.victor.na@gmail.com>
2019-03-17 14:36:19 +0100

    eslint/xo
    Former-commit-id: d76f5b1085b71e9f818eddc86bacb7dd0e9abdee [formerly 89551afb89e8b9fbd52ece8f51dbf5d6a688e138]
    Former-commit-id: 9c6bf2d3f08e67c2541d0b0b15f4082031a9a93d

--- build/make-tiles.mjs
+++ build/make-tiles.mjs
@@ -3,12 +3,12 @@ import child_process from "child_process";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import sharp from "sharp";
 
-const inFilename = process.argv[2],
-    tileSize = 256,
-    mapPath = "src/images/map",
-    iconsPath = "src/images/icons",
-    logoMainFile = `${iconsPath}/logo.png`,
-    icoFile = `${iconsPath}/favicon.ico`;
+const inFilename = process.argv[2];
+const tileSize = 256;
+const mapPath = "src/images/map";
+const iconsPath = "src/images/icons";
+const logoMainFile = `${iconsPath}/logo.png`;
+const icoFile = `${iconsPath}/favicon.ico`;
 
 sharp(inFilename)
     .jpeg({ quality: 100, trellisQuantisation: true, overshootDeringing: true, progressive: false })
@@ -16,28 +16,34 @@ sharp(inFilename)
     .tile({ size: tileSize, layout: "google" })
     .toFile(mapPath)
     .then(
+        // eslint-disable-next-line camelcase
         child_process.exec(`convert ${inFilename} -resize 1024 ${logoMainFile}`, (err, stdout, stderr) => {
             if (err) {
                 throw err;
             }
+
             if (stdout) {
                 console.log("stdout:", stdout);
             }
+
             if (stderr) {
                 console.error("stderr:", stderr);
             }
         })
     )
     .then(
+        // eslint-disable-next-line camelcase
         child_process.exec(
             `convert ${logoMainFile} -define icon:auto-resize=16,32,48,64,128,256 ${icoFile}`,
             (err, stdout, stderr) => {
                 if (err) {
                     throw err;
                 }
+
                 if (stdout) {
                     console.log("stdout:", stdout);
                 }
+
                 if (stderr) {
                     console.error("stderr:", stderr);
                 }
