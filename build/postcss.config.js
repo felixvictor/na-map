efd6269cc0f60dfaa8e60fa722d582a36f7f9438
Felix Victor <felix.victor.na@gmail.com>
2019-04-26 12:51:21 +0200

    crass -> clean-css
    Former-commit-id: 4d83ed0ba547f1545b852b2d82fedab8288fdae9 [formerly d545d4c5de30271ac8e77c6d3cf176c0178f47fd]
    Former-commit-id: b7125b1a9f7e62cb7d91cbcb31222a0892bb0c4a

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,11 +1,10 @@
-const postcssCrassOpt = {
-    optimize: true,
-    pretty: false
+const postcssCleanOpt = {
+    level: { 1: { specialComments: 0 }, 2: {} }
 };
 
 module.exports = ctx => ({
     plugins: {
         autoprefixer: true,
-        "postcss-crass": ctx.env === "production" ? postcssCrassOpt : false
+        "postcss-clean": ctx.env === "production" ? postcssCleanOpt : false
     }
 });
