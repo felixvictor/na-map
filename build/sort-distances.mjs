a6f20174c4345b0c4387f35fc0badfd66b57eca7
Felix Victor <felix.victor.na@gmail.com>
2019-06-09 10:27:49 +0200

    sort-distances clean-up
    Former-commit-id: bc26a52eb3c02254501e2396b6c607e72c214d14 [formerly e58539e631f27aeb29b1ae8e6b66582c7d520cff]
    Former-commit-id: a55286cd754af2160e50dcdec8ff87274a1f1753

--- build/sort-distances.mjs
+++ build/sort-distances.mjs
@@ -1,33 +1,39 @@
 import { cleanName, readJson, saveJson, distancePoints, sortBy, nations } from "./common.mjs";
 
-const date = "2019-04-24";
+const date = "2019-06-08";
 const numPorts = 3;
 const inFilename = `build/API/api-eu1-Ports-${date}.json`;
 const outFilename = "port-distances.json";
 
+const outNations = ["NT"];
+
 const ports = readJson(inFilename);
 
+const findNationName = nationId => nations.find(nation => nation.id === nationId).name;
+
 const portDistances = nations
-    .filter(fromNation => !["NT"].includes(fromNation.short))
+    .filter(fromNation => !outNations.includes(fromNation.short))
     .flatMap(fromNation =>
         ports
+            // fromPort must be a county-capital owned by the fromNation or a free town
             .filter(fromPort =>
                 fromNation.short === "FT"
-                    ? fromPort.Nation === 9
+                    ? fromPort.short === "FT"
                     : fromPort.Name === fromPort.CountyCapitalName && fromPort.Nation === fromNation.id
             )
             .flatMap(fromPort =>
                 ports
+                    // toPort must be a capturable county-capital from another nation than fromPort
                     .filter(
                         toPort =>
                             !toPort.NonCapturable &&
-                            toPort.Nation !== fromPort.Nation &&
-                            toPort.Name === toPort.CountyCapitalName
+                            toPort.Name === toPort.CountyCapitalName &&
+                            toPort.Nation !== fromPort.Nation
                     )
                     .map(toPort => ({
                         fromNation: fromNation.name,
                         fromPort: cleanName(fromPort.Name),
-                        toNation: nations.find(nation => nation.id === toPort.Nation).name,
+                        toNation: findNationName(toPort.Nation),
                         toPort: cleanName(toPort.Name),
                         distance: Math.round(
                             distancePoints(
