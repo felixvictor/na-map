f852505d97373ed5ec361c8b1135d8f30e0e1efd
Felix Victor <felix.victor.na@gmail.com>
2019-06-09 12:34:13 +0200

    sort-distances corrected for Free Towns
    Former-commit-id: c269ce0b9dd06969243bc17ebdfe69fd91760895

--- build/sort-distances.mjs
+++ build/sort-distances.mjs
@@ -11,15 +11,16 @@ const ports = readJson(inFilename);
 
 const findNationName = nationId => nations.find(nation => nation.id === nationId).name;
 
-const portDistances = nations
+const portDistancesNations = nations
     .filter(fromNation => !outNations.includes(fromNation.short))
     .flatMap(fromNation =>
         ports
-            // fromPort must be a county-capital owned by the fromNation or a free town
-            .filter(fromPort =>
-                fromNation.short === "FT"
-                    ? fromPort.short === "FT"
-                    : fromPort.Name === fromPort.CountyCapitalName && fromPort.Nation === fromNation.id
+            // fromPort must be a county-capital owned by the fromNation
+            .filter(
+                fromPort =>
+                    fromPort.Nation !== 9 &&
+                    fromPort.Name === fromPort.CountyCapitalName &&
+                    fromPort.Nation === fromNation.id
             )
             .flatMap(fromPort =>
                 ports
@@ -45,7 +46,42 @@ const portDistances = nations
                     .sort(sortBy(["distance"]))
                     .slice(0, numPorts)
             )
-    )
-    .sort(sortBy(["fromNation", "fromPort", "distance"]));
+    );
+
+const portDistancesFreeTowns = nations
+    .filter(fromNation => !outNations.includes(fromNation.short) && fromNation.short !== "FT")
+    .flatMap(fromNation =>
+        ports
+            // fromPort must be a free town
+            .filter(fromPort => fromPort.Nation === 9)
+            .flatMap(fromPort =>
+                ports
+                    // toPort must be a capturable county-capital from another nation than fromPort
+                    .filter(
+                        toPort =>
+                            !toPort.NonCapturable &&
+                            toPort.Name === toPort.CountyCapitalName &&
+                            toPort.Nation !== fromNation.id
+                    )
+                    .map(toPort => ({
+                        fromNation: fromNation.name,
+                        fromPort: cleanName(fromPort.Name),
+                        toNation: findNationName(toPort.Nation),
+                        toPort: cleanName(toPort.Name),
+                        distance: Math.round(
+                            distancePoints(
+                                { x: fromPort.Position.x, y: fromPort.Position.z },
+                                { x: toPort.Position.x, y: toPort.Position.z }
+                            ) / 1000
+                        )
+                    }))
+                    .sort(sortBy(["distance"]))
+                    .slice(0, numPorts)
+            )
+    );
+
+const portDistances = [...portDistancesNations, ...portDistancesFreeTowns].sort(
+    sortBy(["fromNation", "fromPort", "distance"])
+);
 
 saveJson(outFilename, portDistances);
