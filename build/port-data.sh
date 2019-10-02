9a99aea5445d5e31e1de038c13530e69212cd79c
Felix Victor <felix.victor.na@gmail.com>
2019-10-02 04:51:18 +0200

    port-data.sh: format
    Former-commit-id: 71296f3c70a3a5d6f52d8a29d7da432f099e4f83

--- build/port-data.sh
+++ build/port-data.sh
@@ -135,19 +135,21 @@ function test_for_update () {
 
     local new_file
     local old_file
-	for api_var in "${api_vars[@]}"; do
-	    new_file="${api_base_file}-${server_names[0]}-${api_var}-${server_date}.json"
-		old_file="${api_base_file}-${server_names[0]}-${api_var}-${server_last_date}.json"
 
-		# If old file does not exist test succeeded
-		[[ ! -f "${old_file}" ]] && return 0;
+	  for api_var in "${api_vars[@]}"; do
+        new_file="${api_base_file}-${server_names[0]}-${api_var}-${server_date}.json"
+        old_file="${api_base_file}-${server_names[0]}-${api_var}-${server_last_date}.json"
 
-		# Get new file
-		get_API_data "${server_names[0]}" "${new_file}" "${api_var}"
+        # If old file does not exist test succeeded
+        [[ ! -f "${old_file}" ]] && return 0;
 
-		# Exit if $API_VAR file has not been updated yet
-		cmp --silent "${new_file}" "${old_file}" && { rm "${new_file}"; return 1; }
-	done
+        # Get new file
+        get_API_data "${server_names[0]}" "${new_file}" "${api_var}"
+
+        # Exit if $API_VAR file has not been updated yet
+        cmp --silent "${new_file}" "${old_file}" && { rm "${new_file}"; return 1; }
+    done
+
     return 0
 }
 
