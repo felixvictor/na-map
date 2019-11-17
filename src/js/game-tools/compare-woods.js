a6dd368794711e9c955b5a72ceb71e7b0a7687bd
Felix Victor <felix.victor.na@gmail.com>
2019-11-17 14:12:30 +0100

    javascript ... corrected
    Former-commit-id: f0b4200ebf00c129f0dab801e7e8f50e34360ae0

--- src/js/game-tools/compare-woods.js
+++ src/js/game-tools/compare-woods.js
@@ -353,9 +353,7 @@ export default class CompareWoods {
 
     async _loadAndSetupData() {
         try {
-            this._woodData = await import(/* webpackChunkName: "data-woods" */ "../../gen/woods.json").then(
-                data => data.default
-            );
+            this._woodData = (await import(/* webpackChunkName: "data-woods" */ "../../gen/woods.json")).default;
             this._setupData();
         } catch (error) {
             putImportError(error);
