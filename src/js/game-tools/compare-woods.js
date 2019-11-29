b2f2c8e8e80207692e7edf6bcb0656783f88a0b4
Felix Victor <felix.victor.na@gmail.com>
2019-11-29 17:11:49 +0100

    ship compare: init from clipboard correction wip
    Former-commit-id: 6b8ea1441a7cc10d793c1b3cb141ac67dfcf7298 [formerly 2bd89550187bab2fd1e2991469fac1d972cfac6a]
    Former-commit-id: ef58907c13810c95277b79732da5d5fb5760205d

--- src/js/game-tools/compare-woods.js
+++ src/js/game-tools/compare-woods.js
@@ -313,8 +313,11 @@ export default class CompareWoods {
     }
 
     async woodInit() {
+        console.log("start woodInit");
         await this._loadAndSetupData();
+        console.log("mitte woodInit");
         this._initData();
+        console.log("ende woodInit");
     }
 
     _setupData() {
@@ -351,10 +354,18 @@ export default class CompareWoods {
         this._columns.unshift("Base");
     }
 
+    async _importDefault() {
+        return import(/* webpackChunkName: "data-woods" */ "../../gen/woods.json").then(ns => ns.default);
+    }
+
     async _loadAndSetupData() {
         try {
-            this._woodData = (await import(/* webpackChunkName: "data-woods" */ "../../gen/woods.json")).default;
+            await console.log("start wood _loadAndSetupData");
+            // this._woodData = (await import(/* webpackChunkName: "data-woods" */ "../../gen/woods.json")).default;
+            this._woodData = await this._importDefault();
+            console.log("mitte wood _loadAndSetupData", this._woodData);
             this._setupData();
+            console.log("ende wood _loadAndSetupData");
         } catch (error) {
             putImportError(error);
         }
