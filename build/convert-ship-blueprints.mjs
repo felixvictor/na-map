c22a273a644a86afea7d677e55bbb9324473ded0
Felix Victor <felix.victor.na@gmail.com>
2019-12-08 09:46:02 +0100

    list ship blueprints: seasoned woods added
    Former-commit-id: 339b896df9d36050e97728bbaa9a333880464c19

--- build/convert-ship-blueprints.mjs
+++ build/convert-ship-blueprints.mjs
@@ -8,11 +8,7 @@
  * @license   http://www.gnu.org/licenses/gpl.html
  */
 
-import d3Node from "d3-node";
-const d3n = d3Node();
-const { d3 } = d3n;
-
-import { cleanName, readJson, round, saveJson, sortBy } from "./common.mjs";
+import { cleanName, readJson, saveJson, sortBy } from "./common.mjs";
 
 const inBaseFilename = process.argv[2];
 const outFilename = process.argv[3];
@@ -47,103 +43,103 @@ const itemNames = getItemNames();
 const getShipMass = id => apiItems.find(apiItem => id === apiItem.Id).ShipMass;
 
 /**
+ * Get ship blueprints
+ * @return {object} Ship blueprints
+ */
+const getShipBlueprints = () =>
+    apiItems
+        .filter(apiItem => !apiItem.NotUsed && apiItem.ItemType === "RecipeShip")
+        .map(apiBlueprint => {
+            const shipMass = getShipMass(apiBlueprint.Results[0].Template);
+            return {
+                id: apiBlueprint.Id,
+                name: cleanName(apiBlueprint.Name).replace(" Blueprint", ""),
+                wood: [
+                    { name: "Frame", amount: apiBlueprint.WoodTypeDescs[0].Requirements[0].Amount },
+                    { name: "Planking", amount: Math.round(shipMass * plankingRatio) },
+                    { name: "Crew Space", amount: Math.round(shipMass * crewSpaceRatio) }
+                ],
+                resources: apiBlueprint.FullRequirements.filter(
+                    requirement =>
+                        !(
+                            itemNames.get(requirement.Template).endsWith(" Permit") ||
+                            itemNames.get(requirement.Template) === "Doubloons" ||
+                            itemNames.get(requirement.Template) === "Provisions"
+                        )
+                ).map(requirement => ({
+                    name: itemNames.get(requirement.Template).replace(" Log", ""),
+                    amount: requirement.Amount
+                })),
+                provisions:
+                    (
+                        apiBlueprint.FullRequirements.find(
+                            requirement => itemNames.get(requirement.Template) === "Provisions"
+                        ) || {}
+                    ).Amount || 0,
+                doubloons:
+                    (
+                        apiBlueprint.FullRequirements.find(
+                            requirement => itemNames.get(requirement.Template) === "Doubloons"
+                        ) || {}
+                    ).Amount || 0,
+                permit:
+                    (
+                        apiBlueprint.FullRequirements.find(requirement =>
+                            itemNames.get(requirement.Template).endsWith(" Permit")
+                        ) || {}
+                    ).Amount || 0,
+                ship: {
+                    id: apiBlueprint.Results[0].Template,
+                    name: itemNames.get(apiBlueprint.Results[0].Template),
+                    mass: shipMass
+                },
+                shipyardLevel: apiBlueprint.BuildingRequirements[0].Level + 1,
+                craftLevel: apiBlueprint.RequiresLevel,
+                craftXP: apiBlueprint.GivesXP,
+                labourHours: apiBlueprint.LaborPrice
+            };
+        })
+        // Sort by name
+        .sort(sortBy(["name"]));
+
+const data = getShipBlueprints();
+
+/*
+ * Get resource ratios
+ */
+/*
+const getShipClass = id => apiItems.find(apiItem => id === apiItem.Id).Class;
+const resourceRatios = new Map(data[0].resources.map(resource => [resource.name, []]));
+resourceRatios.set("Frame", []);
+resourceRatios.set("Trim", []);
+const excludedShips = ["GunBoat", "Le Gros Ventre Refit"];
+data.filter(shipBP => !excludedShips.includes(shipBP.name))
+    // .filter(shipBP => getShipClass(shipBP.ship.id) === 5)
+    .forEach(shipBP => {
+        const ratio = shipBP.ship.mass;
+        shipBP.resources.forEach(resource => {
+            const value = round(resource.amount / ratio, 4);
+            resourceRatios.set(resource.name, resourceRatios.get(resource.name).concat(value));
+        });
+        let value = round(shipBP.frames[0].amount / ratio, 4);
+        resourceRatios.set("Frame", resourceRatios.get("Frame").concat(value));
+        value = round(shipBP.trims[0].amount / ratio, 4);
+        resourceRatios.set("Trim", resourceRatios.get("Trim").concat(value));
+        // console.log(`"${shipBP.name}";${ratio}`);
+        console.log(
+            `"${shipBP.name}";${shipBP.resources.map(resource => round(resource.amount / ratio, 4)).join(";")}`
+        );
+    });
+resourceRatios.forEach((value, key) => {
+    console.log(`"${key}";${d3.max(value, d => d)};${d3.median(value)}`);
+});
+*/
+
+/**
  * Convert ship blueprints
  * @return {void}
  */
 const convertShipBlueprints = () => {
-    /**
-     * Get ship blueprints
-     * @return {object} Ship blueprints
-     */
-    const getShipBlueprints = () =>
-        apiItems
-            .filter(apiItem => !apiItem.NotUsed && apiItem.ItemType === "RecipeShip")
-            .map(apiBlueprint => {
-                const shipMass = getShipMass(apiBlueprint.Results[0].Template);
-                return {
-                    id: apiBlueprint.Id,
-                    name: cleanName(apiBlueprint.Name).replace(" Blueprint", ""),
-                    wood: [
-                        { name: "Frame", amount: apiBlueprint.WoodTypeDescs[0].Requirements[0].Amount },
-                        { name: "Planking", amount: Math.round(shipMass * plankingRatio) },
-                        { name: "Crew Space", amount: Math.round(shipMass * crewSpaceRatio) }
-                    ],
-                    resources: apiBlueprint.FullRequirements.filter(
-                        requirement =>
-                            !(
-                                itemNames.get(requirement.Template).endsWith(" Permit") ||
-                                itemNames.get(requirement.Template) === "Doubloons" ||
-                                itemNames.get(requirement.Template) === "Provisions"
-                            )
-                    ).map(requirement => ({
-                        name: itemNames.get(requirement.Template).replace(" Log", ""),
-                        amount: requirement.Amount
-                    })),
-                    provisions:
-                        (
-                            apiBlueprint.FullRequirements.find(
-                                requirement => itemNames.get(requirement.Template) === "Provisions"
-                            ) || {}
-                        ).Amount || 0,
-                    doubloons:
-                        (
-                            apiBlueprint.FullRequirements.find(
-                                requirement => itemNames.get(requirement.Template) === "Doubloons"
-                            ) || {}
-                        ).Amount || 0,
-                    permit:
-                        (
-                            apiBlueprint.FullRequirements.find(requirement =>
-                                itemNames.get(requirement.Template).endsWith(" Permit")
-                            ) || {}
-                        ).Amount || 0,
-                    ship: {
-                        id: apiBlueprint.Results[0].Template,
-                        name: itemNames.get(apiBlueprint.Results[0].Template),
-                        mass: shipMass
-                    },
-                    shipyardLevel: apiBlueprint.BuildingRequirements[0].Level + 1,
-                    craftLevel: apiBlueprint.RequiresLevel,
-                    craftXP: apiBlueprint.GivesXP,
-                    labourHours: apiBlueprint.LaborPrice
-                };
-            })
-            // Sort by name
-            .sort(sortBy(["name"]));
-
-    const data = getShipBlueprints();
-
-    /*
-     * Get resource ratios
-     */
-    /*
-    const getShipClass = id => apiItems.find(apiItem => id === apiItem.Id).Class;
-    const resourceRatios = new Map(data[0].resources.map(resource => [resource.name, []]));
-    resourceRatios.set("Frame", []);
-    resourceRatios.set("Trim", []);
-    const excludedShips = ["GunBoat", "Le Gros Ventre Refit"];
-    data.filter(shipBP => !excludedShips.includes(shipBP.name))
-        // .filter(shipBP => getShipClass(shipBP.ship.id) === 5)
-        .forEach(shipBP => {
-            const ratio = shipBP.ship.mass;
-            shipBP.resources.forEach(resource => {
-                const value = round(resource.amount / ratio, 4);
-                resourceRatios.set(resource.name, resourceRatios.get(resource.name).concat(value));
-            });
-            let value = round(shipBP.frames[0].amount / ratio, 4);
-            resourceRatios.set("Frame", resourceRatios.get("Frame").concat(value));
-            value = round(shipBP.trims[0].amount / ratio, 4);
-            resourceRatios.set("Trim", resourceRatios.get("Trim").concat(value));
-            // console.log(`"${shipBP.name}";${ratio}`);
-            console.log(
-                `"${shipBP.name}";${shipBP.resources.map(resource => round(resource.amount / ratio, 4)).join(";")}`
-            );
-        });
-    resourceRatios.forEach((value, key) => {
-        console.log(`"${key}";${d3.max(value, d => d)};${d3.median(value)}`);
-    });
-    */
-
     saveJson(outFilename, data);
 };
 
