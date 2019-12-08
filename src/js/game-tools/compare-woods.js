effdf8091294f7f1d958021d9dc607a18e6b97d8
Felix Victor <felix.victor.na@gmail.com>
2019-12-08 12:16:08 +0100

    compare woods: table head list cannons: table head list loot: table small
    Former-commit-id: 754d5e4c1b11688b71ce9f38c1e2b699eac32fd8

--- src/js/game-tools/compare-woods.js
+++ src/js/game-tools/compare-woods.js
@@ -72,7 +72,7 @@ class WoodBase extends Wood {
         const middle = 100 / 2;
         let text = '<table class="table table-sm table-striped small mt-4"><thead>';
         text += "<tr>";
-        text += "<tr><th><em>Property</em></th><th><em>Change</em></th></tr></thead><tbody>";
+        text += '<tr><th scope="col">Property</th><th scope="col">Change</th></tr></thead><tbody>';
         wood.properties.forEach((value, key) => {
             text += `<tr><td>${key}</td><td>${
                 value.isPercentage ? formatPercent(value.amount / 100) : formatFloat(value.amount)
@@ -200,8 +200,7 @@ class WoodComparison extends Wood {
         let neutral = 0;
         let diffColour = "";
         let text = '<table class="table table-sm table-striped small wood mt-4"><thead>';
-        text += "<tr>";
-        text += "<tr><th><em>Property</em></th><th><em>Change</em></th></tr></thead><tbody>";
+        text += '<tr><th scope="col">Property</th><th scope="col">Change</th></tr></thead><tbody>';
         wood.properties.forEach((value, key) => {
             text += `<tr><td>${key}</td><td>${getDiff(value.compare, value.base, value.isPercentage)}`;
             text += '<span class="rate">';
