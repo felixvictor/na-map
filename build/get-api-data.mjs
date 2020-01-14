4d251dced03ae4552ffe0295f80a8b3ef401d525
Felix Victor <felix.victor.na@gmail.com>
2020-01-14 19:46:48 +0100

    shell -> node wip: load API data
    Former-commit-id: 11b5c916884a99f8c7488fb53e50121f163d3c6c

--- build/get-api-data.mjs
+++ build/get-api-data.mjs
@@ -1,94 +1,71 @@
-#!/usr/bin/env -S node --experimental-modules
-
 /**
  * This file is part of na-map.
  *
  * @file      Get api data.
  * @module    get-api-data
  * @author    iB aka Felix Victor
- * @copyright 2019
+ * @copyright 2020
  * @license   http://www.gnu.org/licenses/gpl.html
  */
 
 import * as fs from "fs";
 import * as path from "path";
 
-import convert from "xml-js";
-import { getCommonPaths, getServerStartDate, readNAJson, saveJson } from "./common.mjs";
+import {
+    apiBaseFiles,
+    commonPaths,
+    readNAJson,
+    saveJson,
+    serverNames,
+    serverStartDate as serverDate,
+    sortId
+} from "./common.mjs";
 
 const serverBaseName = "cleanopenworldprod";
 const sourceBaseUrl = "https://storage.googleapis.com/";
 const sourceBaseDir = "nacleanopenworldprodshards";
 // http://api.shipsofwar.net/servers?apikey=1ZptRtpXAyEaBe2SEp63To1aLmISuJj3Gxcl5ivl&callback=setActiveRealms
-const serverNames = ["eu1", "eu2"];
 
-/* testbed
-   server_base_name="clean"
-   source_base_url="http://storage.googleapis.com/nacleandevshards/"
-   server_names=(dev)
-*/
+// console.log(commonPaths);
+// console.log(serverDate);
 
-const serverTwitterNames = ["eu1"];
-const apiBaseFiles = ["ItemTemplates", "Ports", "Shops"];
+const deleteFile = fileName => {
+    try {
+        if (fs.existsSync(fileName)) {
+            fs.unlinkSync(fileName);
+        }
+    } catch (error) {
+        throw error;
+    }
+};
 
-/*
-function get_API_data () {
-    local server_name
-    server_name="$1"
-    local out_file
-    out_file="$2"
-    local api_var
-    api_var="$3"
-    local url
-    url="${source_base_url}${api_var}_${server_base_name}${server_name}.json"
-
-    if [[ ! -f "${out_file}" ]]; then
-    if curl --fail --silent --output "${out_file}" "${url}"; then
-    sed -i -e "s/^var $api_var = //; s/\\;$//" "${out_file}"
-else
-    exit $?
-        fi
-        fi
-}
-
-function test_for_update () {
-    local api_base_file
-    api_base_file="$1"
-
-    local new_file
-    local old_file
-
-    for api_var in "${api_vars[@]}"; do
-        new_file="${api_base_file}-${server_names[0]}-${api_var}-${server_date}.json"
-        old_file="${api_base_file}-${server_names[0]}-${api_var}-${server_last_date}.json"
-
-    # If old file does not exist test succeeded
-        [[ ! -f "${old_file}" ]] && return 0;
-
-    # Get new file
-    get_API_data "${server_names[0]}" "${new_file}" "${api_var}"
-
-    # Exit if $API_VAR file has not been updated yet
-    cmp "${new_file}" "${old_file}"
-    cmp --silent "${new_file}" "${old_file}" && { rm "${new_file}"; return 1; }
-    done
-
-    return 0
-}
-*/
+const deleteAPIFiles = fileName => {
+    deleteFile(fileName);
+    deleteFile(`${fileName}.xz`);
+};
 
 const getAPIDataAndSave = async (serverName, apiBaseFile, outFile) => {
     const url = new URL(`${sourceBaseUrl}${sourceBaseDir}/${apiBaseFile}_${serverBaseName}${serverName}.json`);
     const data = await readNAJson(url);
-    console.log(data[0].Name);
-    // saveJson(outFile, data);
-
+    if (data instanceof Error) {
+        console.error(data);
+    } else {
+        data.sort(sortId);
+        console.log(serverName, apiBaseFile, outFile, data[0].Id, data[0].Name);
+        // saveJson(outFile, data);
+    }
 };
 
-const commonPaths = getCommonPaths();
-console.log(commonPaths);
-
-const serverStartDate = getServerStartDate();
-console.log(serverStartDate);
-
-getAPIDataAndSave("eu1", "Ports", "p.json");
+export const getApiData = () => {
+    serverNames.forEach(serverName => {
+        apiBaseFiles.forEach(apiBaseFile => {
+            const outfileName = path.resolve(
+                commonPaths.dirBuild,
+                "API",
+                `${apiBaseFile}-${serverName}-${serverDate}.json`
+            );
+            deleteAPIFiles(outfileName);
+            getAPIDataAndSave(serverName, apiBaseFile, outfileName);
+        });
+    });
+};
