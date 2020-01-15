5c37cb0a1d1e1c5c217f657eeff6a831fbbe9fc2
Felix Victor <felix.victor.na@gmail.com>
2020-01-15 21:54:42 +0100

    shell -> node wip
    Former-commit-id: 6f846ab3027cc6cc9426bad83cb826e00b5fe272 [formerly acad0641999fcc6f732c555396e6edb32a100eac]
    Former-commit-id: 1d2f28760a2d244aa8a8b1eefa66a80c217f6433

--- build/get-api-data.mjs
+++ build/get-api-data.mjs
@@ -10,28 +10,17 @@
 
 import * as fs from "fs";
 import * as path from "path";
-import dayjs from "dayjs";
+import { default as nodeFetch } from "node-fetch";
 
-import {
-    apiBaseFiles,
-    commonPaths,
-    readNAJson,
-    saveJson,
-    serverNames,
-    serverStartDate as serverDate,
-    sortId
-} from "./common.mjs";
+import { apiBaseFiles, saveJson, serverNames, serverStartDate as serverDate, sortId } from "./common.mjs";
 
-const serverBaseName = "cleanopenworldprod";
 const sourceBaseUrl = "https://storage.googleapis.com/";
 const sourceBaseDir = "nacleanopenworldprodshards";
+const serverBaseName = "cleanopenworldprod";
 // http://api.shipsofwar.net/servers?apikey=1ZptRtpXAyEaBe2SEp63To1aLmISuJj3Gxcl5ivl&callback=setActiveRealms
 
 // console.log(commonPaths);
 // console.log(serverDate);
-
-const serverDateYear = String(dayjs(serverDate).year());
-const serverDateMonth = String(dayjs(serverDate).month()).padStart(2, "0");
 
 const deleteFile = fileName => {
     try {
@@ -48,34 +37,45 @@ const deleteAPIFiles = fileName => {
     deleteFile(`${fileName}.xz`);
 };
 
-const getAPIDataAndSave = async (serverName, apiBaseFile, outFile) => {
-    const url = new URL(`${sourceBaseUrl}${sourceBaseDir}/${apiBaseFile}_${serverBaseName}${serverName}.json`);
-    const data = await readNAJson(url);
-    if (data instanceof Error) {
-        console.error(data);
-    } else {
-        data.sort(sortId);
-        console.log(serverName, apiBaseFile, outFile, data[0].Id, data[0].Name);
-        // saveJson(outFile, data);
+const readNAJson = async url => {
+    try {
+        const response = await nodeFetch(url);
+        if (response.ok) {
+            const text = await response.text();
+            const json = await JSON.parse(text.replace(/^var .+ = /, "").replace(/;$/, ""));
+            return json;
+        }
+
+        return new Error(`Cannot load ${url}: ${response.statusText}`);
+    } catch (error) {
+        throw error;
     }
 };
 
-export const getApiData = () => {
+const getAPIDataAndSave = async (serverName, apiBaseFile, outfileName) => {
+    const url = new URL(`${sourceBaseUrl}${sourceBaseDir}/${apiBaseFile}_${serverBaseName}${serverName}.json`);
+    const data = await readNAJson(url);
+    if (data instanceof Error) {
+        throw new TypeError(data);
+    } else {
+        await data.sort(sortId);
+        console.log(serverName, apiBaseFile, outfileName, data[0].Id, data[0].Name);
+        await saveJson(outfileName, data);
+    }
+};
+
+export const getApiData = outBaseFilename => {
     serverNames.forEach(serverName => {
         apiBaseFiles.forEach(apiBaseFile => {
-            const outfileName = path.resolve(
-                commonPaths.dirBuild,
-                "API",
-                serverDateYear,
-                serverDateMonth,
-                `${apiBaseFile}-${serverName}-${serverDate}.json`
-            );
+            const outfileName = path.resolve(outBaseFilename, `${serverName}-${apiBaseFile}-${serverDate}.json`);
+            console.log(outfileName);
             deleteAPIFiles(outfileName);
             getAPIDataAndSave(serverName, apiBaseFile, outfileName);
         });
     });
 };
 
+/*
 const yearRegex = /^api-.+-(\d{4})-\d{2}-\d{2}\.json(\.xz)?$/;
 const monthRegex = /^api-.+-\d{4}-(\d{2})-\d{2}\.json(\.xz)?$/;
 const dirAPI = path.resolve(commonPaths.dirBuild, "API");
@@ -87,16 +87,15 @@ const moveFile = fileName => {
         console.log(year, month);
         const dirNew = path.resolve(dirAPI, year, month);
         console.log(path.resolve(dirAPI, fileName), path.resolve(dirNew, fileName));
-        /*
         fs.rename(path.resolve(dirAPI, fileName), path.resolve(dirNew, fileName), err => {
             if (err) {
                 throw err;
             }
         });
-         */
     }
 };
 
 fs.readdirSync(dirAPI).forEach(fileName => {
     moveFile(fileName);
 });
+*/
