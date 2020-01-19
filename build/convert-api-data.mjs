735b1a0c1d58dc699e699273c49b39ebc456cc14
Felix Victor <felix.victor.na@gmail.com>
2020-01-19 11:44:10 +0100

    shell -> node wip
    Former-commit-id: 33f9c986aa86a39f20b53f3d1e354883998eb31e [formerly a009709225be9664ec3432de774fb0cca79273d1]
    Former-commit-id: e72a01a353a0af86748107b4f3982c6229b366fa

---
+++ build/convert-api-data.mjs
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S node --experimental-modules
+
+/**
+ * This file is part of na-map.
+ *
+ * @file      Convert API data.
+ * @module    convert-api-data
+ * @author    iB aka Felix Victor
+ * @copyright 2020
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+import { convertGenericPortData } from "./convert-generic-port-data.mjs";
+import { convertServerSpecificPortData } from "./convert-server-specific-port-data.mjs";
+
+// import {} from "./common.mjs";
+
+const convertApiData = async () => {
+    console.log("vor convertGenericPortData");
+    convertGenericPortData();
+    console.log("vor convertServerSpecificPortData");
+    convertServerSpecificPortData();
+};
+
+convertApiData();
