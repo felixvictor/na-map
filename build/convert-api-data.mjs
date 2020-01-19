9b5b08f69c5268b02752ab6a59eabc87b0f9f116
Felix Victor <felix.victor.na@gmail.com>
2020-01-19 13:16:39 +0100

    shell -> node wip: compress api data
    Former-commit-id: c478c98986d66da1405785feef9dccbd474e9db6

--- build/convert-api-data.mjs
+++ build/convert-api-data.mjs
@@ -10,16 +10,56 @@
  * @license   http://www.gnu.org/licenses/gpl.html
  */
 
+import * as fs from "fs";
+import * as path from "path";
+import { execSync } from "child_process";
+
 import { convertGenericPortData } from "./convert-generic-port-data.mjs";
 import { convertServerPortData } from "./convert-server-port-data.mjs";
 
-// import {} from "./common.mjs";
+import { apiBaseFiles, baseAPIFilename, serverNames, serverStartDate as serverDate } from "./common.mjs";
 
-const convertApiData = async () => {
+const xz = (command, fileName) => {
+    if (fs.existsSync(fileName)) {
+        execSync(`${command} ${fileName}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`error: ${error.message}`);
+                return;
+            }
+
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+            }
+        });
+    }
+};
+
+const loopApiFiles = command => {
+    const ext = command === "xz" ? "json" : "json.xz";
+
+    for (const serverName of serverNames) {
+        for (const apiBaseFile of apiBaseFiles) {
+            const fileName = path.resolve(baseAPIFilename, `${serverName}-${apiBaseFile}-${serverDate}.${ext}`);
+            xz(command, fileName);
+        }
+    }
+};
+
+const compressApiData = () => {
+    loopApiFiles("xz");
+};
+
+const uncompressApiData = () => {
+    loopApiFiles("unxz");
+};
+
+const convertApiData = () => {
     console.log("vor convertGenericPortData");
     convertGenericPortData();
     console.log("vor convertServerPortData");
     convertServerPortData();
 };
 
+uncompressApiData();
 convertApiData();
+compressApiData();
