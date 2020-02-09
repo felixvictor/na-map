50caaa840a37847b40dbf0b6f4a0053b3839d85d
Felix Victor <felix.victor.na@gmail.com>
2020-02-09 13:45:32 +0100

    mjs -> ts (first files)

--- src/node/move-api-files.ts
+++ src/node/move-api-files.ts
@@ -1,10 +1,8 @@
-#!/usr/bin/env -S yarn yarn node --experimental-modules --no-warnings
-
 /**
  * This file is part of na-map.
  *
  * @file      Move api files to directories (year|month).
- * @module    move-api-files
+ * @module    src/node/move-api-files
  * @author    iB aka Felix Victor
  * @copyright 2020
  * @license   http://www.gnu.org/licenses/gpl.html
@@ -13,7 +11,7 @@
 import * as fs from "fs"
 import * as path from "path"
 
-import { commonPaths, makeDirAsync } from "./common.mjs"
+import { commonPaths, makeDirAsync } from "./common"
 
 const yearRegex = /^api-.+-(\d{4})-\d{2}-\d{2}\.json(\.xz)?$/
 const monthRegex = /^api-.+-\d{4}-(\d{2})-\d{2}\.json(\.xz)?$/
@@ -23,7 +21,7 @@ const monthRegex = /^api-.+-\d{4}-(\d{2})-\d{2}\.json(\.xz)?$/
  * @param {string} oldFileName
  * @param {string} newFileName
  */
-const moveFileAsync = (oldFileName, newFileName) => {
+const moveFileAsync = (oldFileName: string, newFileName: string): void => {
     fs.rename(oldFileName, newFileName, err => {
         if (err) {
             throw err
@@ -36,17 +34,16 @@ const moveFileAsync = (oldFileName, newFileName) => {
  * @param {string} fileName
  * @return {Promise<void>}
  */
-const moveAPIFile = async fileName => {
+const moveAPIFile = async (fileName: string): Promise<void> => {
     // Consider only file to match regex "api-...")
-    if (fileName.match(yearRegex)) {
-        const year = fileName.match(yearRegex)[1]
-        const month = fileName.match(monthRegex)[1]
-
+    const year = fileName.match(yearRegex)?.[1]
+    const month = fileName.match(monthRegex)?.[1]
+    if (year && month) {
         const dirNew = path.resolve(commonPaths.dirAPI, year, month)
         const fileNameNew = fileName.replace("api-", "")
         await makeDirAsync(dirNew)
 
-        console.log("--", path.resolve(commonPaths.dirAPI, fileName), path.resolve(dirNew, fileNameNew))
+        console.log("-> ", path.resolve(commonPaths.dirAPI, fileName), path.resolve(dirNew, fileNameNew))
         await moveFileAsync(path.resolve(commonPaths.dirAPI, fileName), path.resolve(dirNew, fileNameNew))
     }
 }
