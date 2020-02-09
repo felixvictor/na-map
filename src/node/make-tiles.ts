cfce3873b4719d6bb9cb6146030689973409f637
Felix Victor <felix.victor.na@gmail.com>
2020-02-09 13:45:32 +0100

    mjs -> ts (first files)

--- src/node/make-tiles.ts
+++ src/node/make-tiles.ts
@@ -1,16 +1,14 @@
-#!/usr/bin/env -S yarn yarn yarn node --experimental-modules --no-warnings
-
 /**
  * This file is part of na-map.
  *
  * @file      Make map tiles.
- * @module    game-tools/list-modules
+ * @module    src/node/make-tiles
  * @author    iB aka Felix Victor
- * @copyright 2018-2019
+ * @copyright 2018, 2019, 2020
  * @license   http://www.gnu.org/licenses/gpl.html
  */
 
-// eslint-disable-next-line camelcase
+// eslint-disable-next-line camelcase,@typescript-eslint/camelcase
 import child_process from "child_process"
 import sharp from "sharp"
 
@@ -20,7 +18,7 @@ const mapPath = "src/images/map"
 const iconsPath = "src/images/icons"
 const logoMainFile = `${iconsPath}/logo.png`
 
-const convert = async () => {
+const convert = async (): Promise<void> => {
     try {
         await sharp(inFilename)
             .webp({ quality: 100, reductionEffort: 6 })
@@ -28,7 +26,7 @@ const convert = async () => {
             .tile({ size: tileSize, layout: "google" })
             .toFile(mapPath)
 
-        // eslint-disable-next-line camelcase
+        // eslint-disable-next-line camelcase,@typescript-eslint/camelcase
         await child_process.exec(`convert ${inFilename} -resize 1024 ${logoMainFile}`, (err, stdout, stderr) => {
             if (err) {
                 throw err
