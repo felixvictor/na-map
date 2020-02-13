b62796f17e2d5ec0e5b6e3de5ad7aa45b98320cb
Felix Victor <felix.victor.na@gmail.com>
2020-02-13 19:17:28 +0100

    js -> ts
    Former-commit-id: 291d3f17684538d4501e5854004f1ccb5aed3a76

---
+++ src/js/json-types.ts
@@ -0,0 +1,418 @@
+/**
+ * This file is part of na-map.
+ *
+ * @file      Json types.
+ * @module    json-types
+ * @author    iB aka Felix Victor
+ * @copyright 2020
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+// https://jvilk.com/MakeTypes/
+
+import { Point } from "../node/common"
+
+/****************************
+ * buildings.json
+ */
+
+export interface Building {
+    id: number
+    name: string
+    resource: BuildingResource[]
+    batch?: BuildingBatch | null[] | null
+    levels?: BuildingLevelsEntity[] | null
+    byproduct?: null[] | null
+}
+export interface BuildingResource {
+    name: string
+    price: number
+}
+export interface BuildingBatch {
+    price: number
+    amount: number
+    labour: number
+}
+export interface BuildingLevelsEntity {
+    labourDiscount: number
+    production: number
+    maxStorage: number
+    price: number
+    materials?: (BuildingMaterialsEntity | null)[] | null
+}
+export interface BuildingMaterialsEntity {
+    item: string
+    amount: number
+}
+
+/****************************
+ * cannons.json
+ */
+
+export interface Cannon {
+    medium: CannonEntity[]
+    long: CannonEntity[]
+    carronade: CannonEntity[]
+}
+export interface CannonEntity {
+    name: string
+    damage: CannonDamage
+    traverse: CannonTraverse
+    dispersion: CannonDispersion
+    generic: CannonGeneric
+    "penetration(m)": CannonPenetration
+}
+export interface CannonDamage {
+    basic: CannonValue
+    "reload time": CannonValue
+    splinter: CannonValue
+    "per second": CannonValue
+}
+export interface CannonValue {
+    value: number
+    digits: number
+}
+export interface CannonTraverse {
+    up: CannonValue
+    down: CannonValue
+}
+export interface CannonDispersion {
+    horizontal: CannonValue
+    vertical: CannonValue
+}
+export interface CannonGeneric {
+    weight: CannonValue
+    crew: CannonValue
+}
+export interface CannonPenetration {
+    50: CannonValue
+    100: CannonValue
+    250: CannonValue
+    500: CannonValue
+    750: CannonValue
+    1000: CannonValue
+}
+
+/****************************
+ * loot.json
+ */
+
+export interface Loot {
+    loot: LootLootEntity[]
+    chests: LootChestsEntity[]
+}
+export interface LootLootEntity {
+    id: number
+    name: string
+    items: LootItemsEntity[]
+}
+export interface LootItemsEntity {
+    id: number
+    name: string
+    chance: number
+    amount: LootAmount
+}
+export interface LootAmount {
+    min: number
+    max: number
+}
+export interface LootChestsEntity {
+    id: number
+    name: string
+    weight: number
+    lifetime: number
+    items: LootItemsEntity[]
+}
+
+/****************************
+ * modules.json
+ */
+
+export interface ModuleEntity {
+    id: number
+    name: string
+    usageType: string
+    moduleLevel: string
+    properties?: ModulePropertiesEntity
+    type: string
+}
+export interface ModulePropertiesEntity {
+    modifier: string
+    amount: number
+    isPercentage: boolean
+}
+export type Module = string | ModuleEntity[]
+
+/****************************
+ * nations.json
+ */
+
+export interface OwnershipPortsPerNation {
+    date: string
+    NT: number
+    PR: number
+    ES: number
+    FR: number
+    GB: number
+    VP: number
+    DK: number
+    SE: number
+    US: number
+    RU: number
+    DE: number
+    PL: number
+}
+
+/****************************
+ * ownership.json
+ */
+
+export interface Ownership {
+    region: string
+    data: OwnershipGroup[]
+}
+export interface OwnershipGroup {
+    group: string
+    data: OwnershipLabel[]
+}
+export interface OwnershipLabel {
+    label: string
+    data: OwnershipLabelRange[]
+}
+export interface OwnershipLabelRange {
+    timeRange: string[]
+    val: string
+    labelVal: string
+}
+
+/****************************
+ * pb-zones.json
+ */
+
+export interface PbZone {
+    id: number
+    position: Point
+    pbCircles: Point[]
+    forts: Point[]
+    towers: Point[]
+    joinCircles: Point[]
+    raidCircles: Point[]
+    raidPoints: Point[]
+}
+
+/****************************
+ * ports.json
+ */
+
+export interface Port {
+    id: number
+    name: string
+    coordinates: Point
+    angle: number
+    textAnchor: string
+    region: string
+    countyCapitalName: string
+    county: string
+    countyCapital: boolean
+    shallow: boolean
+    availableForAll: boolean
+    brLimit: number
+    portPoints: number
+    portBattleStartTime: number
+    portBattleType: string
+    nonCapturable: boolean
+    conquestMarksPension: number
+}
+
+/****************************
+ * prices.json
+ */
+
+export interface Price {
+    standard: PriceStandardWood[]
+    seasoned: PriceSeasonedWood[]
+}
+export interface PriceStandardWood {
+    name: string
+    real: number
+    labour: number
+}
+export interface PriceSeasonedWood {
+    name: string
+    real: number
+    labour: number
+    doubloon: number
+    tool: number
+}
+
+/****************************
+ * recipes.json
+ */
+
+export interface Recipe {
+    recipe: RecipeEntity[]
+    ingredient: RecipeIngredientEntity[]
+}
+export interface RecipeEntity {
+    id: number
+    name: string
+    labourPrice: number
+    goldPrice: number
+    itemRequirements: RecipeItemRequirement[]
+    result: RecipeResult
+    craftGroup: string
+    serverType: string
+}
+export interface RecipeItemRequirement {
+    name: string
+    amount: number
+}
+export interface RecipeResult {
+    id: number
+    name: string
+    amount: number
+}
+export interface RecipeIngredientEntity {
+    id: number
+    name: string
+    recipeNames: string[]
+}
+
+/****************************
+ * ship-blueprints.json
+ */
+
+export interface ShipBlueprint {
+    id: number
+    name: string
+    wood: ShipBlueprintResource[]
+    resources: ShipBlueprintResource[]
+    provisions: number
+    doubloons: number
+    permit: number
+    ship: ShipBlueprintShip
+    shipyardLevel: number
+    craftLevel: number
+    craftXP: number
+    labourHours: number
+}
+export interface ShipBlueprintResource {
+    name: string
+    amount: number
+}
+export interface ShipBlueprintShip {
+    id: number
+    name: string
+    mass: number
+}
+
+/****************************
+ * ships.json
+ */
+
+export interface Ship {
+    id: number
+    name: string
+    class: number
+    gunsPerDeck: number[]
+    guns: number
+    broadside: ShipBroadside
+    deckClassLimit: number[]
+    shipMass: number
+    battleRating: number
+    decks: number
+    holdSize: number
+    maxWeight: number
+    crew: ShipCrew
+    speedDegrees: number[]
+    speed: ShipSpeed
+    sides: ShipHealth
+    bow: ShipHealth
+    stern: ShipHealth
+    structure: ShipStructureOrPump
+    sails: ShipSails
+    pump: ShipStructureOrPump
+    rudder: ShipRudder
+    upgradeXP: number
+    repairTime: ShipRepairTime
+    ship: ShipShip
+    mast: ShipMast
+    premium: boolean
+    tradeShip: boolean
+}
+export interface ShipBroadside {
+    cannons: number
+    carronades: number
+}
+export interface ShipCrew {
+    min: number
+    max: number
+    sailing: number
+}
+export interface ShipSpeed {
+    min: number
+    max: number
+}
+export interface ShipHealth {
+    armour: number
+    thickness: number
+}
+export interface ShipStructureOrPump {
+    armour: number
+}
+export interface ShipSails {
+    armour: number
+    risingSpeed: number
+}
+export interface ShipRudder {
+    armour: number
+    turnSpeed: number
+    halfturnTime: number
+    thickness: number
+}
+export interface ShipRepairTime {
+    stern: number
+    bow: number
+    sides: number
+    rudder: number
+    sails: number
+    structure: number
+}
+export interface ShipShip {
+    waterlineHeight: number
+    firezoneHorizontalWidth: number
+    structureLeaksPerSecond: number
+    deceleration: number
+    acceleration: number
+    turningAcceleration: number
+    turningYardAcceleration: number
+    maxSpeed: number
+}
+export interface ShipMast {
+    bottomArmour: number
+    middleArmour: number
+    topArmour: number
+    bottomThickness: number
+    middleThickness: number
+    topThickness: number
+}
+
+/****************************
+ * woods.json
+ */
+
+export interface Wood {
+    trim: WoodTrimEntityOrFrameEntity[]
+    frame: WoodTrimEntityOrFrameEntity[]
+}
+export interface WoodTrimEntityOrFrameEntity {
+    id: number
+    properties: WoodProperty[]
+    type: string
+    name: string
+}
+export interface WoodProperty {
+    modifier: string
+    amount: number
+    isPercentage: boolean
+}
