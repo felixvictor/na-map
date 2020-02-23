6174192fd618bcbc49446c99b9b46eafe26a16f0
Felix Victor <felix.victor.na@gmail.com>
2020-02-23 12:49:58 +0100

    js -> ts
    Former-commit-id: de8eea9d4fb023067d60310f3e77b2c9963a30b7 [formerly 9240368b105550c30c2306635059c3851841fcc1]
    Former-commit-id: b1c7f10e30e95fbb104448002ad624269218ced4

--- src/js/node/convert-api-data.ts
+++ src/js/node/convert-api-data.ts
@@ -8,9 +8,7 @@
  * @license   http://www.gnu.org/licenses/gpl.html
  */
 
-import * as fs from "fs"
-import * as path from "path"
-import { execSync } from "child_process"
+import { compressApiData, uncompressApiData } from "./common-node"
 
 import { convertBuildingData } from "./convert-buildings"
 import { convertCannons } from "./convert-cannons"
@@ -24,40 +22,14 @@ import { convertServerPortData } from "./convert-server-port-data"
 import { convertShipData } from "./convert-ship-data"
 import { convertOwnershipData } from "./convert-ownership"
 import { createPortBattleSheet } from "./create-pb-sheets"
-*/
-import { apiBaseFiles, serverNames } from "../common"
-import { baseAPIFilename, serverStartDate as serverDate } from "./common-node"
+ */
 
 const runType = process.argv[2] || "client"
-
-const xz = (command: string, fileName: string): void => {
-    if (fs.existsSync(fileName)) {
-        execSync(`${command} ${fileName}`)
-    }
-}
-
-const loopApiFiles = (command: string): void => {
-    const ext = command === "xz" ? "json" : "json.xz"
-
-    for (const serverName of serverNames) {
-        for (const apiBaseFile of apiBaseFiles) {
-            const fileName = path.resolve(baseAPIFilename, `${serverName}-${apiBaseFile}-${serverDate}.${ext}`)
-            xz(command, fileName)
-        }
-    }
-}
-
-const compressApiData = () => {
-    loopApiFiles("xz")
-}
-
-const uncompressApiData = () => {
-    loopApiFiles("unxz")
-}
 
 const convertApiData = async (): Promise<void> => {
     convertBuildingData()
     convertCannons()
+    /*
     convertGenericPortData()
     convertLootData()
     convertModules()
@@ -68,10 +40,12 @@ const convertApiData = async (): Promise<void> => {
     if (runType.endsWith("server")) {
         convertOwnershipData()
     }
+
+     */
 }
 
 uncompressApiData()
 // noinspection JSIgnoredPromiseFromCall
 convertApiData()
-createPortBattleSheet()
+//createPortBattleSheet()
 compressApiData()
