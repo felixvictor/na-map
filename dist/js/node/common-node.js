0ed8ec95cafca21554ad79991feeb650a69425c1
Felix Victor <felix.victor.na@gmail.com>
2020-02-24 19:15:11 +0100

    js -> ts
    Former-commit-id: 15e447f734dfc15e211ba24a8994e288b691e8ed [formerly e15a479373aa3242649dcf795b2124a600eed1a7]
    Former-commit-id: 17c581daf5f0c86c557f2a2bd81876de049d27ef

---
+++ dist/js/node/common-node.js
@@ -0,0 +1,91 @@
+import { exec, execSync } from "child_process";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import { promisify } from "util";
+const execP = promisify(exec);
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+dayjs.extend(utc);
+import { apiBaseFiles, serverNames } from "../common";
+export const serverMaintenanceHour = 10;
+export const distanceMapSize = 4096;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirOut = path.resolve(__dirname, "..", "public", "data");
+const dirBuild = path.resolve(__dirname, "..", "..", "..", "build");
+const dirAPI = path.resolve(dirBuild, "API");
+const dirModules = path.resolve(dirBuild, "Modules");
+const dirSrc = path.resolve(__dirname, "..", "src");
+const dirDist = path.resolve(__dirname, "..", "..");
+const dirGenServer = path.resolve(dirDist, "gen-server");
+const dirGenGeneric = path.resolve(dirDist, "gen-generic");
+export const commonPaths = {
+    dirAPI,
+    dirBuild,
+    dirGenGeneric,
+    dirGenServer,
+    dirModules,
+    dirOut,
+    dirSrc,
+    fileTwitterRefreshId: path.resolve(dirAPI, "response-id.txt"),
+    filePbSheet: path.resolve(dirGenServer, "port-battle.xlsx"),
+    fileBuilding: path.resolve(dirGenGeneric, "buildings.json"),
+    fileCannon: path.resolve(dirGenGeneric, "cannons.json"),
+    fileLoot: path.resolve(dirGenGeneric, "loot.json"),
+    fileModules: path.resolve(dirGenGeneric, "modules.json"),
+    fileNation: path.resolve(dirGenGeneric, "nations.json"),
+    fileOwnership: path.resolve(dirGenGeneric, "ownership.json"),
+    filePbZone: path.resolve(dirGenGeneric, "pb-zones.json"),
+    filePort: path.resolve(dirGenGeneric, "ports.json"),
+    filePrices: path.resolve(dirGenGeneric, "prices.json"),
+    fileRecipe: path.resolve(dirGenGeneric, "recipes.json"),
+    fileRepair: path.resolve(dirGenGeneric, "repairs.json"),
+    fileShip: path.resolve(dirGenGeneric, "ships.json"),
+    fileShipBlueprint: path.resolve(dirGenGeneric, "ship-blueprints.json"),
+    fileWood: path.resolve(dirGenGeneric, "woods.json")
+};
+const getServerStartDateTime = () => {
+    let serverStart = dayjs()
+        .utc()
+        .hour(serverMaintenanceHour)
+        .minute(0)
+        .second(0);
+    if (dayjs.utc().isBefore(serverStart)) {
+        serverStart = dayjs.utc(serverStart).subtract(1, "day");
+    }
+    return serverStart;
+};
+export const serverStartDateTime = getServerStartDateTime().format("YYYY-MM-DD HH:mm");
+export const serverStartDate = getServerStartDateTime().format("YYYY-MM-DD");
+const serverDateYear = String(dayjs(serverStartDate).year());
+const serverDateMonth = String(dayjs(serverStartDate).month() + 1).padStart(2, "0");
+export const baseAPIFilename = path.resolve(commonPaths.dirAPI, serverDateYear, serverDateMonth);
+const fileExistsAsync = async (fileName) => !!(await fs.promises.stat(fileName).catch(() => false));
+export const xzAsync = async (command, fileName) => {
+    const fileExists = await fileExistsAsync(fileName);
+    if (fileExists) {
+        await execP(`${command} ${fileName}`);
+    }
+    return true;
+};
+const xz = (command, fileName) => {
+    if (fs.existsSync(fileName)) {
+        execSync(`${command} ${fileName}`);
+    }
+};
+const loopApiFiles = (command) => {
+    const ext = command === "xz" ? "json" : "json.xz";
+    for (const serverName of serverNames) {
+        for (const apiBaseFile of apiBaseFiles) {
+            const fileName = path.resolve(baseAPIFilename, `${serverName}-${apiBaseFile}-${serverStartDate}.${ext}`);
+            xz(command, fileName);
+        }
+    }
+};
+export const compressApiData = () => {
+    loopApiFiles("xz");
+};
+export const uncompressApiData = () => {
+    loopApiFiles("unxz");
+};
