57b53a0339ee587c848d35798ad61fa8405a3257
Felix Victor <felix.victor.na@gmail.com>
2020-03-14 20:01:28 +0100

    js -> ts
    Former-commit-id: bd7d163f28322b737a30676b0d6e7eb925ad4d85 [formerly 8f0a2359eaa02d0e46beffc7c120e408d61bd269]
    Former-commit-id: 2658d733dfe942ac02c28d7f2eddf8c1c075ba35

---
+++ dist/js/common/servers.js
@@ -0,0 +1,14 @@
+/*!
+ * This file is part of na-map.
+ *
+ * @file      Game server data.
+ * @module    servers
+ * @author    iB aka Felix Victor
+ * @copyright 2019, 2020
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+export const servers = [
+    { id: "eu1", name: "War", type: "PVP" },
+    { id: "eu2", name: "Peace", type: "PVE" }
+];
+//# sourceMappingURL=servers.js.map
