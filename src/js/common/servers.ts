ce9270c66ea1025327517932f2b9bbee0334221f
Felix Victor <felix.victor.na@gmail.com>
2020-03-16 13:47:15 +0100

    js -> ts: next try with webpack.config.js
    Former-commit-id: eab12921507e72f10cfd2b51a09f49e6f982c9a7 [formerly 750782ee864373044e766daef3cd169624565df3]
    Former-commit-id: 237739bc865971f2dfc710366cab7924518e98ac

--- src/js/common/servers.ts
+++ src/js/common/servers.ts
@@ -14,7 +14,9 @@ export interface Server {
     type: string
 }
 
-export const servers = [
+const servers = [
     { id: "eu1", name: "War", type: "PVP" },
     { id: "eu2", name: "Peace", type: "PVE" }
 ]
+
+module.exports = servers
