df54be8f63bea59441d713d1ffd142841d2494f6
Felix Victor <felix.victor.na@gmail.com>
2020-03-17 18:18:03 +0100

    js -> ts
    Former-commit-id: 6c704ddde554ea18df8e9164da0a0a27b18426bf

---
+++ src/js/common/interface.d.ts
@@ -0,0 +1,20 @@
+/*!
+ * This file is part of na-map.
+ *
+ * @file      Common types.
+ * @module    common-types.d
+ * @author    iB aka Felix Victor
+ * @copyright 2020
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SVGSVGDatum {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface SVGGDatum {}
+
+export interface MinMaxCoord {
+    min: number
+    max: number
+}
