fa07f359da06387beb19f961bc247e7fd7861c2e
Felix Victor <felix.victor.na@gmail.com>
2020-04-15 20:28:55 +0200

    js -> ts: list ships works
    Former-commit-id: b79222892411d4d264ca3a9c31385a00e9a96f9c

--- src/js/browser/game-tools/list-ships.ts
+++ src/js/browser/game-tools/list-ships.ts
@@ -10,40 +10,50 @@
 
 /// <reference types="bootstrap" />
 import "bootstrap/js/dist/util"
-/// <reference types="bootstrap" />
 import "bootstrap/js/dist/modal"
 import { select as d3Select } from "d3-selection"
-import { default as Tablesort } from "tablesort"
+// import { default as Tablesort } from "tablesort"
 
 import { registerEvent } from "../analytics"
-import { initTablesort, insertBaseModal } from "../../common/interfaces"
-import { putImportError, sortBy } from "../util"
-import { formatFloatFixed, formatInt } from "../../common/common-format";
+import { putImportError } from "../../common/common"
+import { HtmlString, initTablesort, insertBaseModal } from "../../common/common-browser"
+import { formatFloatFixed, formatInt } from "../../common/common-format"
+import { sortBy } from "../../common/common-node"
+
+import { ShipData } from "../../common/gen-json"
 
 /**
  *
  */
 export default class ShipList {
+    private readonly _baseName: string
+    private readonly _baseId: HtmlString
+    private readonly _buttonId: HtmlString
+    private readonly _modalId: HtmlString
+    private _shipData: ShipData[] = {} as ShipData[]
+
     constructor() {
         this._baseName = "List ships"
         this._baseId = "ship-list"
-        this._buttonId =`button-${this._baseId}`
-        this._modalId =`modal-${this._baseId}`
+        this._buttonId = `button-${this._baseId}`
+        this._modalId = `modal-${this._baseId}`
+
         this._setupListener()
     }
 
-    async _loadAndSetupData() {
+    async _loadAndSetupData(): Promise<void> {
         try {
-            this._shipData = (await import(/* webpackChunkName: "data-ships" */ "~Lib/gen-generic/ships.json")).default
+            this._shipData = (await import(/* webpackChunkName: "data-ships" */ "Lib/gen-generic/ships.json"))
+                .default as ShipData[]
         } catch (error) {
             putImportError(error)
         }
     }
 
-    _setupListener() {
+    _setupListener(): void {
         let firstClick = true
 
-        document.getElementById(this._buttonId).addEventListener("click", async event => {
+        document.querySelector(`#${this._buttonId}`)?.addEventListener("click", async (event) => {
             if (firstClick) {
                 firstClick = false
                 await this._loadAndSetupData()
@@ -55,25 +65,25 @@ export default class ShipList {
         })
     }
 
-    _injectModal() {
-        insertBaseModal(this._modalId, this._baseName, "lg")
+    _injectModal(): void {
+        insertBaseModal({ id: this._modalId, title: this._baseName, size: "lg" })
 
         d3Select(`#${this._modalId} .modal-body`)
             .append("div")
-            .attr("id",`${this._baseId}`)
+            .attr("id", `${this._baseId}`)
             .attr("class", "container-fluid")
     }
 
-    _initModal() {
+    _initModal(): void {
         initTablesort()
         this._injectModal()
 
         this._injectList()
     }
 
-    _shipListSelected() {
+    _shipListSelected(): void {
         // If the modal has no content yet, insert it
-        if (!document.getElementById(this._modalId)) {
+        if (!document.querySelector(`#${this._modalId}`)) {
             this._initModal()
         }
 
@@ -83,27 +93,25 @@ export default class ShipList {
 
     /**
      * Show ships
-     * @return {void}
-     * @private
      */
-    _injectList() {
+    _injectList(): void {
         d3Select(`#${this._baseId} div`).remove()
 
         // Add new recipe list
-        d3Select(`#${this._baseId}`)
-            .append("div")
-            .classed("row ingredients", true)
+        d3Select(`#${this._baseId}`).append("div").classed("row ingredients", true)
         d3Select(`#${this._baseId} div`).html(this._getList())
-        const table = document.getElementById(`table-${this._baseId}`)
+        /*
+        const table = document.querySelector(`#table-${this._baseId}`)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const sortTable = new Tablesort(table)
+        */
     }
 
     // noinspection FunctionTooLongJS
-    _getList() {
+    _getList(): HtmlString {
         let text = ""
 
-        text +=`<table id="table-${this._baseId}" class="table table-sm small tablesort"><thead>`
+        text += `<table id="table-${this._baseId}" class="table table-sm small tablesort"><thead>`
         text += "<tr>"
         text += '<th scope="col" class="border-bottom-0"></th>'
         text += '<th scope="col" class="border-bottom-0"></th>'
@@ -132,21 +140,21 @@ export default class ShipList {
         text += "</tr></thead><tbody>"
 
         const ships = this._shipData
-            .filter(ship => !ship.name.startsWith("Rookie "))
+            .filter((ship) => !ship.name.startsWith("Rookie "))
             .sort(sortBy(["class", "-battleRating", "name"]))
         for (const ship of ships) {
             text += "<tr>"
-            text +=`<td class="text-right">${ship.class}</td>`
-            text +=`<td>${ship.name}</td>`
-            text +=`<td class="text-right">${ship.guns}</td>`
-            text +=`<td class="text-right">${ship.battleRating}</td>`
-            text +=`<td class="text-right">${ship.crew.max}</td>`
-            text +=`<td class="text-right">${formatFloatFixed(ship.ship.maxSpeed)}</td>`
-            text +=`<td class="text-right">${formatFloatFixed(ship.rudder.turnSpeed)}</td>`
-            text +=`<td class="text-right">${ship.broadside.cannons}</td>`
-            text +=`<td class="text-right">${ship.gunsPerDeck[4] ? ship.gunsPerDeck[4] : ""}</td>`
-            text +=`<td class="text-right">${ship.gunsPerDeck[5] ? ship.gunsPerDeck[5] : ""}</td>`
-            text +=`<td class="text-right" data-sort="${ship.sides.armour}">${formatInt(ship.sides.armour)} (${
+            text += `<td class="text-right">${ship.class}</td>`
+            text += `<td>${ship.name}</td>`
+            text += `<td class="text-right">${ship.guns}</td>`
+            text += `<td class="text-right">${ship.battleRating}</td>`
+            text += `<td class="text-right">${ship.crew.max}</td>`
+            text += `<td class="text-right">${formatFloatFixed(ship.ship.maxSpeed)}</td>`
+            text += `<td class="text-right">${formatFloatFixed(ship.rudder.turnSpeed)}</td>`
+            text += `<td class="text-right">${ship.broadside.cannons}</td>`
+            text += `<td class="text-right">${ship.gunsPerDeck[4] ? ship.gunsPerDeck[4] : ""}</td>`
+            text += `<td class="text-right">${ship.gunsPerDeck[5] ? ship.gunsPerDeck[5] : ""}</td>`
+            text += `<td class="text-right" data-sort="${ship.sides.armour}">${formatInt(ship.sides.armour)} (${
                 ship.sides.thickness
             })</td>`
             text += "</tr>"
