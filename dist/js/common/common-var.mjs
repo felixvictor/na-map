b84992ed8e858c27751aebd3da7e7df0b65c9b55
Felix Victor <felix.victor.na@gmail.com>
2020-05-22 17:38:39 +0200

    dist
    Former-commit-id: 59b52b754e044281e78757ecd1ab964760478019 [formerly 8a8fdfcd2e0a2a7f8b4d0674f79f28de64e6ed60]
    Former-commit-id: 82676021881c94318e5561a3becc6ebed083dc9e

--- dist/js/common/common-var.mjs
+++ dist/js/common/common-var.mjs
@@ -12,5 +12,6 @@ export const apiBaseFiles = ["ItemTemplates", "Ports", "Shops"];
 export const serverNames = ["eu1", "eu2"];
 export const serverTwitterNames = ["eu1"];
 export const serverMaintenanceHour = 10;
-export const distanceMapSize = 4096;
+export const mapSize = 8192;
+export const distanceMapSize = 8192;
 //# sourceMappingURL=common-var.js.map
