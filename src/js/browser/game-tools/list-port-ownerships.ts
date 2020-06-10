f412b3af4ee2a961ff930878698349925556ad49
Felix Victor <felix.victor.na@gmail.com>
2020-06-10 12:38:59 +0200

    ownership.json: reduced size

--- src/js/browser/game-tools/list-port-ownerships.ts
+++ src/js/browser/game-tools/list-port-ownerships.ts
@@ -31,13 +31,13 @@ import {
     stack as d3Stack,
     stackOffsetNone as d3StackOffsetNone,
 } from "d3-shape"
-import TimelinesChart, { Group } from "timelines-chart"
+import TimelinesChart from "timelines-chart"
 
 import { registerEvent } from "../analytics"
 import { NationFullName, nations, NationShortName, putImportError } from "../../common/common"
 import { colourList, getContrastColour, HtmlString, insertBaseModal } from "../../common/common-browser"
 
-import { Ownership, OwnershipNation } from "../../common/gen-json"
+import { Group, Ownership, OwnershipNation } from "../../common/gen-json";
 
 /**
  *
@@ -85,7 +85,6 @@ export default class ListPortOwnerships {
         try {
             this._nationData = (await import(/* webpackChunkName: "data-nations" */ "Lib/gen-generic/nations.json"))
                 .default as Array<OwnershipNation<number>>
-            // @ts-expect-error
             this._ownershipData = (
                 await import(/* webpackChunkName: "data-ownership" */ "Lib/gen-generic/ownership.json")
             ).default as Ownership[]
@@ -100,7 +99,7 @@ export default class ListPortOwnerships {
     _setupListener(): void {
         let firstClick = true
 
-        document.querySelector(`#${this._buttonId}`)?.addEventListener("click", async (event) => {
+        document.querySelector(`#${this._buttonId}`)?.addEventListener("click", async () => {
             if (firstClick) {
                 firstClick = false
                 await this._loadAndSetupData()
@@ -302,6 +301,7 @@ export default class ListPortOwnerships {
      */
     _injectChart(data: Group[]): void {
         TimelinesChart()
+            // @ts-expect-error
             .data(data)
             .enableAnimations(false)
             .timeFormat("%-d %B %Y")
