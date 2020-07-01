b6518b12007a4cb69b091432e8b24bddb7d55d72
Felix Victor <felix.victor.na@gmail.com>
2020-07-01 16:28:05 +0200

    squash! update

--- dist/js/browser/game-tools/compare-ships/compare-ships.js
+++ dist/js/browser/game-tools/compare-ships/compare-ships.js
@@ -660,7 +660,6 @@ export class CompareShips {
     }
     _getShipData(columnId) {
         const shipDataDefault = this._shipData.find((ship) => ship.id === this._shipIds[columnId]);
-        shipDataDefault.crew.carronades = shipDataDefault.crew.cannons;
         shipDataDefault.boarding = {
             attack: 0,
             cannonsAccuracy: 0,
