b7916df8d83675ca267d52225681428aca8f63d7
Felix Victor <felix.victor.na@gmail.com>
2020-07-19 18:00:15 +0200

    dist

--- dist/js/node/convert-server-port-data.mjs
+++ dist/js/node/convert-server-port-data.mjs
@@ -140,20 +140,30 @@ const setAndSaveDroppedItems = async (serverName) => {
     });
     await saveJsonAsync(path.resolve(commonPaths.dirGenServer, `${serverName}-items.json`), items);
 };
-const ticks = 621355968000000000;
+const baseTimeInTicks = 621355968000000000;
+const getTimeFromTicks = (timeInTicks) => {
+    return dayjs.utc((timeInTicks - baseTimeInTicks) / 10000).format("YYYY-MM-DD HH:mm");
+};
 const setAndSavePortBattleData = async (serverName) => {
     const pb = apiPorts
-        .map((port) => ({
-        id: Number(port.Id),
-        name: cleanName(port.Name),
-        nation: nations[port.Nation].short,
-        capturer: port.Capturer,
-        lastPortBattle: dayjs((port.LastPortBattle - ticks) / 10000).format("YYYY-MM-DD HH:mm"),
-        attackerNation: "",
-        attackerClan: "",
-        attackHostility: 0,
-        portBattle: "",
-    }))
+        .map((port) => {
+        const portData = {
+            id: Number(port.Id),
+            name: cleanName(port.Name),
+            nation: nations[port.Nation].short,
+        };
+        if (port.Capturer !== "") {
+            portData.capturer = port.Capturer;
+        }
+        if (port.LastPortBattle > 0) {
+            portData.captured = getTimeFromTicks(port.LastPortBattle);
+        }
+        else if (port.LastRaidStartTime > 0) {
+            portData.captured = getTimeFromTicks(port.LastRaidStartTime);
+            portData.capturer = "RAIDER";
+        }
+        return portData;
+    })
         .sort(sortBy(["id"]));
     await saveJsonAsync(path.resolve(commonPaths.dirGenServer, `${serverName}-pb.json`), pb);
 };
