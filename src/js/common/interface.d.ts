a1a0100e9e37d44710132b8989066b26b088b159
Felix Victor <felix.victor.na@gmail.com>
2020-07-26 09:41:52 +0200

    display port: patrol zones

--- src/js/common/interface.d.ts
+++ src/js/common/interface.d.ts
@@ -40,6 +40,7 @@ export interface NestedIndex<T> {
 
 export type HtmlResult = VNode<unknown> | Array<VNode<unknown>>
 
+export type SVGString = string
 export type HtmlString = string
 export interface BaseModal {
     id: HtmlString
