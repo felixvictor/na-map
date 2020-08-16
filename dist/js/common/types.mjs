df41664097ef5b289b932fd0e1675e7b773a96ff
Felix Victor <felix.victor.na@gmail.com>
2020-08-16 11:37:00 +0200

    dist

--- dist/js/common/types.mjs
+++ dist/js/common/types.mjs
@@ -1,4 +1,5 @@
 const frontlinesType = ["attacking", "defending"];
 export const lootType = ["loot", "chests", "items"];
 const woodColumnType = ["Base", "C1", "C2", "C3"];
+export const portBonusType = ["crew", "gunnery", "hull", "mast", "sailing"];
 //# sourceMappingURL=types.js.map
