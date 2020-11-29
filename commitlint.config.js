c98e454d12819e2a24f22722461e085447be06bd
Felix Victor <felix.victor.na@gmail.com>
2020-11-29 16:59:23 +0100

    build(commitlint): allow subject in sentence case

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,7 @@ module.exports = {
     extends: ["@commitlint/config-conventional"],
     ignores: [(message) => message.includes("WIP")],
     rules: {
+        "subject-case": [2, "never", ["start-case", "pascal-case", "upper-case"]],
         "type-enum": [
             2,
             "always",
