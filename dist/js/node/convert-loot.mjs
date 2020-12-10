2e359ac0a626e0bda6d062c3a5110fbcd16b46ae
Felix Victor <felix.victor.na@gmail.com>
2020-12-10 11:07:38 +0100

    build: update dist

--- dist/js/node/convert-loot.mjs
+++ dist/js/node/convert-loot.mjs
@@ -13,8 +13,9 @@ import { readJson, saveJsonAsync } from "../common/common-file";
 import { getOrdinal } from "../common/common-math";
 import { cleanName, sortBy } from "../common/common-node";
 import { serverIds } from "../common/servers";
-let apiItems;
 const secondsPerHour = 3600;
+let apiItems;
+let itemNames;
 const getLootName = (classId, isMission) => {
     if (classId === -1) {
         return "Trader bot (open world)";
@@ -28,39 +29,34 @@ const getLootItemName = (name, type) => {
     }
     return cleanedName;
 };
+const getLootItems = (types) => apiItems.filter((item) => !item.NotUsed && types.includes(item.ItemType));
+const getLootItemsChance = (chestLootTableId) => {
+    const lootTable = apiItems.filter((item) => Number(item.Id) === chestLootTableId);
+    return lootTable[0].Items[0].Chance;
+};
+const getLootContent = (lootItems, itemProbability) => lootItems.map((item) => ({
+    id: Number(item.Template),
+    name: itemNames.get(Number(item.Template)) ?? "",
+    chance: itemProbability.length > 0 ? Number(itemProbability[Number(item.Chance)]) : Number(item.Chance),
+    amount: { min: Number(item.Stack?.Min), max: Number(item.Stack?.Max) },
+}));
+const getChestItems = (lootItems) => lootItems.map((item) => ({
+    id: Number(item.Template),
+    name: itemNames.get(Number(item.Template)) ?? "",
+    amount: { min: Number(item.Stack?.Min), max: Number(item.Stack?.Max) },
+}));
+const getChestItemsFromChestLootTable = (chestLootTableId) => apiItems.filter((item) => Number(item.Id) === chestLootTableId).flatMap((item) => getChestItems(item.Items ?? []));
 const convertLoot = async () => {
-    const getItemNames = () => new Map(apiItems.map((item) => [Number(item.Id), getLootItemName(item.Name, item.ItemType)]));
-    const itemNames = getItemNames();
-    const getLootItemsChance = (chestLootTableId) => {
-        const lootTable = apiItems.filter((item) => Number(item.Id) === chestLootTableId);
-        return lootTable[0].Items[0].Chance;
-    };
-    const getLootItems = (lootItems, itemProbability) => lootItems.map((item) => ({
-        id: Number(item.Template),
-        name: itemNames.get(Number(item.Template)) ?? "",
-        chance: itemProbability.length > 0 ? Number(itemProbability[Number(item.Chance)]) : Number(item.Chance),
-        amount: { min: Number(item.Stack?.Min), max: Number(item.Stack?.Max) },
-    }));
-    const getChestItems = (lootItems) => lootItems.map((item) => ({
-        id: Number(item.Template),
-        name: itemNames.get(Number(item.Template)) ?? "",
-        amount: { min: Number(item.Stack?.Min), max: Number(item.Stack?.Max) },
-    }));
-    const getChestItemsFromChestLootTable = (chestLootTableId) => apiItems
-        .filter((item) => Number(item.Id) === chestLootTableId)
-        .flatMap((item) => getChestItems(item.Items ?? []));
     const data = {};
-    let types = ["ShipLootTableItem"];
-    const loot = apiItems.filter((item) => !item.NotUsed && types.includes(item.ItemType));
+    const loot = getLootItems(["ShipLootTableItem"]);
     data.loot = loot
         .map((item) => ({
         id: Number(item.Id),
         name: getLootName(Number(item.Class), item.EventLootTable),
-        items: getLootItems(item.Items ?? [], item.itemProbability ?? [0]).sort(sortBy(["chance", "id"])),
+        items: getLootContent(item.Items ?? [], item.itemProbability ?? [0]).sort(sortBy(["chance", "id"])),
     }))
         .sort(sortBy(["id"]));
-    types = ["TimeBasedConvertibleItem"];
-    const chests = apiItems.filter((item) => !item.NotUsed && types.includes(item.ItemType));
+    const chests = getLootItems(["TimeBasedConvertibleItem"]);
     data.chests = chests
         .map((item) => ({
         id: Number(item.Id),
@@ -77,6 +73,7 @@ const convertLoot = async () => {
 };
 export const convertLootData = () => {
     apiItems = readJson(path.resolve(baseAPIFilename, `${serverIds[0]}-ItemTemplates-${serverDate}.json`));
+    itemNames = new Map(apiItems.map((item) => [Number(item.Id), getLootItemName(item.Name, item.ItemType)]));
     void convertLoot();
 };
 //# sourceMappingURL=convert-loot.js.map
