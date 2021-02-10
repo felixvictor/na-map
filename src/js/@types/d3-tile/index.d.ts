cdc593ab2e4636661b435a924f8695c98ce0a4eb
Felix Victor <felix.victor.na@gmail.com>
2021-02-10 14:42:58 +0100

    WIP: show power map - d3-tile types

---
+++ src/js/@types/d3-tile/index.d.ts
@@ -0,0 +1,44 @@
+declare module "d3-tile" {
+    import { ZoomTransform } from "d3-zoom"
+
+    export type Tile = [number, number, number]
+
+    export interface Tiles extends ArrayLike<Tile> {
+        [index: number]: Tile
+        translate: [number, number]
+        scale: number
+    }
+
+    export interface TileFn extends Function {
+        tile(scale?: number, translate?: ZoomTransform): Tiles
+
+        size(): [number, number]
+        size(size: [number, number]): this
+
+        extent(): [[number, number], [number, number]]
+        extent(extent: [[number, number], [number, number]]): this
+
+        scale(scale: number): this
+        scale(scale?: () => number): number
+
+        translate(translate?: () => ZoomTransform): [number, number]
+        translate(translate: ZoomTransform): this
+
+        zoomDelta(): number
+        zoomDelta(zoomDelta: number): this
+
+        tileSize(): number
+        tileSize(tileSize: number): this
+
+        clamp(): boolean
+        clamp(clamp: boolean): this
+
+        clampX(): boolean
+        clampX(clamp: boolean): this
+
+        clampY(): boolean
+        clampY(clamp: boolean): this
+    }
+
+    export function tile(): TileFn
+}
