fb2942567797110769eecda443b92256e11021fa
Felix Victor <felix.victor.na@gmail.com>
2021-04-22 11:02:38 +0200

    WIP: layout - list loot

---
+++ src/js/browser/game-tools/list-loot/index.ts
@@ -0,0 +1,306 @@
+/*!
+ * This file is part of na-map.
+ *
+ * @file      List loot and chests.
+ * @module    game-tools/list-loot
+ * @author    iB aka Felix Victor
+ * @copyright Felix Victor 2017 to 2021
+ * @license   http://www.gnu.org/licenses/gpl.html
+ */
+
+import { h, render } from "preact"
+import htm from "htm"
+
+import { registerEvent } from "../../analytics"
+import { formatInt } from "common/common-format"
+
+import {
+    Loot,
+    LootAmount,
+    LootChestsEntity,
+    LootLootItemsEntity,
+    LootLootEntity,
+    LootTypeList,
+    LootChestItemsEntity,
+} from "common/gen-json"
+import { lootType, LootType } from "common/types"
+import { HtmlResult, HtmlString } from "common/interface"
+import { sortBy } from "common/common"
+import ListLootSelect from "./select"
+import { getIdFromBaseName } from "common/common-browser"
+import Select from "util/select"
+import { LootItemMap, SourceDetail } from "list-loot"
+import Modal from "util/modal"
+
+const html = htm.bind(h)
+
+export default class ListLoot {
+    #data = {} as Loot
+    #items: LootItemMap = new Map()
+    #modal: Modal | undefined = undefined
+    #optionItems: LootItemMap = new Map()
+    #select = {} as LootTypeList<ListLootSelect>
+    #selectedItemId = 0
+    #selectedType: LootType = "" as LootType
+    readonly #baseId: HtmlString
+    readonly #baseName = "List loot and chests"
+    readonly #menuId: HtmlString
+    readonly #regularLootTypes = new Set<LootType>(["chest", "fish", "loot"])
+    readonly #types: LootType[] = lootType
+
+    constructor() {
+        this.#baseId = getIdFromBaseName(this.#baseName)
+        this.#menuId = `menu-${this.#baseId}`
+
+        this._setupListener()
+    }
+
+    static _printAmount(amount: LootAmount): string {
+        return amount.min === amount.max
+            ? formatInt(amount.min)
+            : `${formatInt(amount.min)} to ${formatInt(amount.max)}`
+    }
+
+    static _printChance(chance: number): string {
+        return formatInt((1 - chance) * 100)
+    }
+
+    async _loadAndSetupData(): Promise<void> {
+        const sourceData = (
+            await import(/* webpackChunkName: "data-loot" */ "../../../../../lib/gen-generic/loot.json")
+        ).default as Loot
+
+        for (const type of this.#types) {
+            this.#data[type] = sourceData[type]
+        }
+    }
+
+    async _menuClicked(): Promise<void> {
+        registerEvent("Tools", this.#baseName)
+
+        if (this.#modal) {
+            this.#modal.show()
+        } else {
+            await this._loadAndSetupData()
+            this.#modal = new Modal(this.#baseName, "md")
+            this._setupSelect()
+            this._setupSelectListener()
+        }
+    }
+
+    _setupListener(): void {
+        document.querySelector(`#${this.#menuId}`)?.addEventListener("click", () => {
+            void this._menuClicked()
+        })
+    }
+
+    _getTypeOptions(type: LootType): HtmlString {
+        return this.#data[type]
+            .sort(
+                sortBy<LootLootEntity | LootChestsEntity, "id" | "name">(["name"])
+            )
+            .map((item: LootLootEntity | LootChestsEntity) => `<option value="${item.id}">${item.name}</option>`)
+            .join("")
+    }
+
+    _setOptionItems(
+        source: LootLootEntity | LootChestsEntity,
+        item: LootLootItemsEntity | LootChestItemsEntity,
+        chance: number
+    ): void {
+        const sourceDetail = new Map<number, SourceDetail>([
+            [source.id, { id: source.id, name: source.name, chance, amount: item.amount }],
+        ])
+
+        this.#optionItems.set(item.id, {
+            name: item.name,
+            sources: this.#optionItems.has(item.id)
+                ? new Map([...this.#optionItems.get(item.id)!.sources, ...sourceDetail])
+                : sourceDetail,
+        })
+    }
+
+    _getItemsOptions(): HtmlString {
+        // Loot and fish
+        for (const loot of [...this.#data.loot, ...this.#data.fish] as LootLootEntity[]) {
+            const { items } = loot
+            for (const item of items) {
+                this._setOptionItems(loot, item, item.chance)
+            }
+        }
+
+        // Chests
+        for (const chest of this.#data.chest as LootChestsEntity[]) {
+            for (const group of chest.itemGroup) {
+                const { chance, items } = group
+                for (const item of items) {
+                    this._setOptionItems(chest, item, chance)
+                }
+            }
+        }
+
+        // Sort by name
+        this.#items = new Map([...this.#optionItems.entries()].sort((a, b) => a[1].name.localeCompare(b[1].name)))
+
+        return [...this.#items].map((value) => `<option value="${value[0]}">${value[1].name}</option>`).join("")
+    }
+
+    _getOptions(type: LootType): HtmlString {
+        let h = ""
+
+        if (this.#regularLootTypes.has(type)) {
+            h = this._getTypeOptions(type)
+        } else if (type === "item") {
+            h = this._getItemsOptions()
+        }
+
+        return h
+    }
+
+    _setupSelect(): void {
+        for (const type of this.#types) {
+            const title = type === "loot" ? "ship loot" : type === "fish" ? "fishing region" : type
+            const selectpickerOptions: BootstrapSelectOptions = {
+                dropupAuto: false,
+                liveSearch: true,
+                liveSearchNormalize: true,
+                liveSearchPlaceholder: "Search ...",
+                title: `Select ${title}`,
+                virtualScroll: true,
+            }
+
+            this.#select[type] = new ListLootSelect(
+                this.#baseId,
+                this.#modal!.selectsSel,
+                selectpickerOptions,
+                type,
+                this._getOptions(type)
+            )
+        }
+    }
+
+    _setupSelectListener(): void {
+        for (const type of this.#types) {
+            this.#select[type].getSelect$().on("change", () => {
+                this.#selectedType = type
+                this._itemSelected()
+            })
+        }
+    }
+
+    _getLootItemsText(
+        items: Array<LootLootItemsEntity | LootChestItemsEntity>,
+        title: string,
+        chance = true
+    ): HtmlResult {
+        return html`
+            <table class="table table-sm small na-table no-sort">
+                <thead>
+                    <tr>
+                        <th scope="col">${title}</th>
+                        ${chance ? html`<th scope="col" class="text-end">Chance (0 − 100)</th>` : ""}
+                        <th scope="col" class="text-end">Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${items.map(
+                        (item) =>
+                            html`
+                                <tr>
+                                    <td>${item.name}</td>
+                                    ${chance
+                                        ? html` <td class="text-end">
+                                              ${ListLoot._printChance((item as LootLootItemsEntity).chance)}
+                                          </td>`
+                                        : ""}
+                                    <td class="text-end">${ListLoot._printAmount(item.amount)}</td>
+                                </tr>
+                            `
+                    )}
+                </tbody>
+            </table>
+        `
+    }
+
+    _getSourceText(): HtmlResult {
+        const items = this.#items.get(this.#selectedItemId)
+
+        if (items) {
+            const sources = [...items.sources].map((value) => value[1]).sort(sortBy(["chance", "name"]))
+
+            return html`${this._getLootItemsText(sources, "Source")}`
+        }
+
+        return html``
+    }
+
+    _getChestsText(): HtmlResult {
+        const currentChest = (this.#data.chest as LootChestsEntity[]).find((item) => item.id === this.#selectedItemId)
+
+        if (currentChest) {
+            return html`
+                <p>
+                    Weight ${formatInt(currentChest.weight)} tons<br />Lifetime ${formatInt(currentChest.lifetime)}
+                    hours
+                </p>
+                ${currentChest.itemGroup.map(
+                    (group) => html` <h5>Group chance: ${ListLoot._printChance(group.chance)}</h5>
+                        ${this._getLootItemsText(group.items.sort(sortBy(["name"])), "Item", false)}`
+                )}
+            `
+        }
+
+        return html``
+    }
+
+    _getLootText(): HtmlResult {
+        const currentItem = (this.#data[this.#selectedType] as LootLootEntity[]).find(
+            (item) => item.id === this.#selectedItemId
+        )
+
+        if (currentItem) {
+            const items = currentItem.items.sort(sortBy(["chance", "name"]))
+
+            return html`${this._getLootItemsText(items, "Item")}`
+        }
+
+        return html``
+    }
+
+    _getText(): HtmlResult {
+        if (this.#selectedType === "item") {
+            return this._getSourceText()
+        }
+
+        if (this.#selectedType === "chest") {
+            return this._getChestsText()
+        }
+
+        return this._getLootText()
+    }
+
+    /**
+     * Construct item table
+     */
+    _getTable(): HtmlResult {
+        return html` <div class="mt-4">${this._getText()}</div> `
+    }
+
+    _resetOtherSelects(): void {
+        const types = this.#types.filter((type) => type !== this.#selectedType)
+        for (const type of types) {
+            Select.reset(this.#select[type].getSelect$())
+        }
+    }
+
+    /**
+     * Show items for selected loot or chest
+     */
+    _itemSelected(): void {
+        this.#selectedItemId = Number(this.#select[this.#selectedType].getSelectedValues())
+        const div = this.#modal!.outputSel
+
+        this._resetOtherSelects()
+        render(this._getTable(), div.node() as HTMLElement)
+    }
+}
