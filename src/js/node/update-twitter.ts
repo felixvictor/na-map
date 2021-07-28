cc7a99b43edcedb082982f4c4be0bdddc2598325
Felix Victor <felix.victor.na@gmail.com>
2021-07-28 15:23:56 +0200

    fix(get tweets): port ownership changes only after last maintenance

--- src/js/node/update-twitter.ts
+++ src/js/node/update-twitter.ts
@@ -375,7 +375,13 @@ const checkCooldown = (tweet: string): boolean => {
     let result: RegExpExecArray | null
     let matched = true
 
-    if ((result = defendedRegex.exec(tweet)) !== null) {
+    if ((result = capturedRegex.exec(tweet)) !== null) {
+        isPortDataChanged = true
+        cooldownOn(result)
+    } else if ((result = npcCapturedRegex.exec(tweet)) !== null) {
+        isPortDataChanged = true
+        cooldownOn(result)
+    } else if ((result = defendedRegex.exec(tweet)) !== null) {
         isPortDataChanged = true
         cooldownOn(result)
         // eslint-disable-next-line no-negated-condition
@@ -465,15 +471,11 @@ const updatePorts = async (): Promise<void> => {
 
         matched = checkFlags(tweet)
 
-        if (
-            tweetTime.isAfter(dayjs.utc(currentServerStartDateTime).subtract(2, "day")) &&
-            tweetTime.isBefore(dayjs.utc(currentServerStartDateTime).subtract(1, "day"))
-        ) {
+        if (tweetTime.isAfter(dayjs.utc(currentServerStartDateTime).subtract(2, "day"))) {
             matched = matched || checkCooldown(tweet)
-        } else if (
-            tweetTime.isAfter(dayjs.utc(currentServerStartDateTime).subtract(1, "day")) &&
-            tweetTime.isBefore(dayjs.utc(currentServerStartDateTime))
-        ) {
+        }
+
+        if (tweetTime.isAfter(dayjs.utc(currentServerStartDateTime).subtract(1, "day"))) {
             matched = matched || checkPBAndRaid(tweet)
         } else if (tweetTime.isAfter(dayjs.utc(currentServerStartDateTime))) {
             matched = matched || checkPort(tweet)
