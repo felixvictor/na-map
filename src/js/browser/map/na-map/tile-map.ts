ada58c507b0ec4dcbffb618cf94ce20b10f36159
Felix Victor <felix.victor.na@gmail.com>
2022-02-20 15:37:04 +0100

    refactor: map zoom, tile map optimisation

--- src/js/browser/map/na-map/tile-map.ts
+++ src/js/browser/map/na-map/tile-map.ts
@@ -1,12 +1,19 @@
 import { select as d3Select, Selection } from "d3-selection"
-import { Tile, tile as d3Tile, TileFn, Tiles } from "d3-tile"
+import { tile as d3Tile, TileFn, Tiles } from "d3-tile"
 import { zoomIdentity as d3ZoomIdentity, ZoomTransform } from "d3-zoom"
+
 import { maxTileScale, tileSize } from "common/common-var"
 
+import { Point } from "common/common-math"
+
+type TileKey = string
+
 export default class TileMap {
+    #currentScale = 0
+    #currentTiles = new Map<TileKey, Point>()
     #gMap = {} as Selection<SVGGElement, Event, HTMLElement, unknown>
     #mapTileTransform = {} as ZoomTransform
-    #tileG = {} as Selection<SVGImageElement, Tile, SVGGElement, unknown>
+    #tileG = {} as Selection<SVGImageElement, [TileKey, Point], SVGGElement, unknown>
     #tiler: TileFn
 
     constructor(width: number, height: number) {
@@ -28,26 +35,41 @@ export default class TileMap {
         this.#tileG = this.#gMap.selectAll("image")
     }
 
-    #updateMap(data: Tile[]): void {
-        this.#gMap.attr("transform", this.#mapTileTransform.toString())
+    #updateMap(data: Map<TileKey, Point>): void {
         this.#tileG = this.#tileG
-            .data(data, ([x, y, z]) => `${z}/${y}/${x}`)
-            .join("image")
-            .attr("xlink:href", ([x, y, z]) => `images/map/${z}/${y}/${x}.webp`)
-            .attr("x", ([x]) => x)
-            .attr("y", ([, y]) => y)
-            .attr("width", 1)
-            .attr("height", 1)
+            .data([...data].sort(), ([key]) => key)
+            .join((enter) =>
+                enter
+                    .append("image")
+                    .attr("xlink:href", ([key]) => `images/map/${key}.webp`)
+                    .attr("x", ([, [x]]) => x)
+                    .attr("y", ([, [, y]]) => y)
+                    .attr("width", 1)
+                    .attr("height", 1)
+            )
     }
 
     drawMap(transform: ZoomTransform): void {
         // Zoom in at scale larger than maxTileZoom
         const zoomDelta = transform.k <= maxTileScale ? 0 : Math.log2(maxTileScale) - Math.log2(transform.k)
+
         this.#tiler.zoomDelta(zoomDelta)
-
         const tiles = this.#tiler(transform) as Tiles
-        this.#mapTileTransform = d3ZoomIdentity.scale(tiles.scale).translate(tiles.translate[0], tiles.translate[1])
 
-        this.#updateMap(tiles)
+        this.#mapTileTransform = d3ZoomIdentity.scale(tiles.scale).translate(tiles.translate[0], tiles.translate[1])
+        this.#gMap.attr("transform", this.#mapTileTransform.toString())
+
+        const map = tiles.map(([x, y, z]): [TileKey, Point] => [`${z}/${y}/${x}`, [x, y]])
+        if (transform.k !== this.#currentScale) {
+            this.#currentScale = transform.k
+            this.#tileG.selectAll("image").remove()
+            this.#currentTiles = new Map(map)
+        } else {
+            // Join all old and new tiles
+            const join = new Map([...map, ...this.#currentTiles])
+            this.#currentTiles = join
+        }
+
+        this.#updateMap(this.#currentTiles)
     }
 }
